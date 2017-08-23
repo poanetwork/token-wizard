83db8d1081752e9dfa596019bb609b16f3b8f039
viktor <baranov.viktor.27@gmail.com>
2017-08-23 16:11:43 +0300

    Whitelist fields temp

--- src/components/stepThree.js
+++ src/components/stepThree.js
@@ -9,7 +9,7 @@ import { InputField } from './Common/InputField'
 import { NAVIGATION_STEPS, defaultState, VALIDATION_MESSAGES, VALIDATION_TYPES, TEXT_FIELDS } from '../utils/constants'
 const { CROWDSALE_SETUP } = NAVIGATION_STEPS
 const { EMPTY, VALID, INVALID } = VALIDATION_TYPES
-const { START_TIME, END_TIME, RATE, SUPPLY, WALLET_ADDRESS } = TEXT_FIELDS
+const { START_TIME, END_TIME, RATE, SUPPLY, WALLET_ADDRESS, CROWDSALE_SETUP_NAME, ADDRESS, MIN, MAX } = TEXT_FIELDS
 
 export class stepThree extends stepTwo {
   constructor(props) {
@@ -65,65 +65,158 @@ export class stepThree extends stepTwo {
   render() {
     const { validations } = this.state
     console.log('333 validations', validations)
-    return (
-      <section className="steps steps_crowdsale-contract" ref="three">
-        <StepNavigation activeStep={CROWDSALE_SETUP}/>
-        <div className="steps-content container">
-          <div className="about-step">
-            <div className="step-icons step-icons_crowdsale-setup"></div>
-            <p className="title">Crowdsale setup</p>
-            <p className="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+    if (this.state.contractType == this.state.contractTypes.standard) {
+      return (
+        <section className="steps steps_crowdsale-contract" ref="three">
+          <StepNavigation activeStep={CROWDSALE_SETUP}/>
+          <div className="steps-content container">
+            <div className="about-step">
+              <div className="step-icons step-icons_crowdsale-setup"></div>
+              <p className="title">Crowdsale setup</p>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+            <div className="hidden">
+              <InputField 
+                side='left' 
+                type='datetime-local' 
+                title={START_TIME} 
+                value={this.state.crowdsale.startTime} 
+                valid={validations.startTime} 
+                errorMessage={VALIDATION_MESSAGES.START_TIME} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'startTime')}/>
+              <InputField 
+                side='right' 
+                type='datetime-local' 
+                title={END_TIME} 
+                value={this.state.crowdsale.endTime} 
+                valid={validations.endTime} 
+                errorMessage={VALIDATION_MESSAGES.END_TIME} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'endTime')}/>
+              <InputField 
+                side='left' 
+                type='text' 
+                title={WALLET_ADDRESS} 
+                value={this.state.crowdsale.walletAddress} 
+                valid={validations.walletAddress} 
+                errorMessage={VALIDATION_MESSAGES.WALLET_ADDRESS} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'walletAddress')}/>
+              <InputField 
+                side='right' 
+                type='number' 
+                title={SUPPLY} 
+                value={this.state.crowdsale.supply} 
+                valid={validations.supply} 
+                errorMessage={VALIDATION_MESSAGES.SUPPLY} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'supply')}/>
+              <InputField 
+                side='left' 
+                type='number' 
+                title={RATE} 
+                value={this.state.pricingStrategy.rate} 
+                valid={validations.rate} 
+                errorMessage={VALIDATION_MESSAGES.RATE} 
+                onChange={(e) => this.changeState(e, 'pricingStrategy', 'rate')}/>
+            </div>
           </div>
-          <div className="hidden">
-            <InputField 
-              side='left' 
-              type='datetime-local' 
-              title={START_TIME} 
-              value={this.state.crowdsale.startTime} 
-              valid={validations.startTime} 
-              errorMessage={VALIDATION_MESSAGES.START_TIME} 
-              onChange={(e) => this.changeState(e, 'crowdsale', 'startTime')}/>
-            <InputField 
-              side='right' 
-              type='datetime-local' 
-              title={END_TIME} 
-              value={this.state.crowdsale.endTime} 
-              valid={validations.endTime} 
-              errorMessage={VALIDATION_MESSAGES.END_TIME} 
-              onChange={(e) => this.changeState(e, 'crowdsale', 'endTime')}/>
-            <InputField 
-              side='left' 
-              type='text' 
-              title={WALLET_ADDRESS} 
-              value={this.state.crowdsale.walletAddress} 
-              valid={validations.walletAddress} 
-              errorMessage={VALIDATION_MESSAGES.WALLET_ADDRESS} 
-              onChange={(e) => this.changeState(e, 'crowdsale', 'walletAddress')}/>
-            <InputField 
-              side='right' 
-              type='number' 
-              title={SUPPLY} 
-              value={this.state.crowdsale.supply} 
-              valid={validations.supply} 
-              errorMessage={VALIDATION_MESSAGES.SUPPLY} 
-              onChange={(e) => this.changeState(e, 'crowdsale', 'supply')}/>
-            <InputField 
-              side='left' 
-              type='number' 
-              title={RATE} 
-              value={this.state.pricingStrategy.rate} 
-              valid={validations.rate} 
-              errorMessage={VALIDATION_MESSAGES.RATE} 
-              onChange={(e) => this.changeState(e, 'pricingStrategy', 'rate')}/>
+          <div className="button-container">
+            {this.renderLinkComponent()}
           </div>
-        </div>
-        <div className="button-container">
-          {this.renderLinkComponent()}
-        </div>
-      </section>
-    )}
+        </section>
+      )
+    } else if (this.state.contractType == this.state.contractTypes.whitelistwithcap) {
+      return (
+        <section className="steps steps_crowdsale-contract" ref="three">
+          <StepNavigation activeStep={CROWDSALE_SETUP}/>
+          <div className="steps-content container">
+            <div className="about-step">
+              <div className="step-icons step-icons_crowdsale-setup"></div>
+              <p className="title">Crowdsale setup</p>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+            <div className="hidden">
+              <InputField 
+                side='left' 
+                type='text' 
+                title={CROWDSALE_SETUP_NAME} 
+                value='ICO'/>
+              <InputField 
+                side='right' 
+                type='text' 
+                title={WALLET_ADDRESS} 
+                value={this.state.crowdsale.walletAddress} 
+                valid={validations.walletAddress} 
+                errorMessage={VALIDATION_MESSAGES.WALLET_ADDRESS} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'walletAddress')}/>
+              <InputField 
+                side='left' 
+                type='datetime-local' 
+                title={START_TIME} 
+                value={this.state.crowdsale.startTime} 
+                valid={validations.startTime} 
+                errorMessage={VALIDATION_MESSAGES.START_TIME} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'startTime')}/>
+              <InputField 
+                side='right' 
+                type='datetime-local' 
+                title={END_TIME} 
+                value={this.state.crowdsale.endTime} 
+                valid={validations.endTime} 
+                errorMessage={VALIDATION_MESSAGES.END_TIME} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'endTime')}/>
+              <InputField 
+                side='right' 
+                type='number' 
+                title={SUPPLY} 
+                value={this.state.crowdsale.supply} 
+                valid={validations.supply} 
+                errorMessage={VALIDATION_MESSAGES.SUPPLY} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'supply')}/>
+              <InputField 
+                side='left' 
+                type='number' 
+                title={RATE} 
+                value={this.state.pricingStrategy.rate} 
+                valid={validations.rate} 
+                errorMessage={VALIDATION_MESSAGES.RATE} 
+                onChange={(e) => this.changeState(e, 'pricingStrategy', 'rate')}/>
+            </div>
+            <div className="white-list-title">
+              <p className="title">Whitelist</p>
+            </div>
+            <div className="white-list-item-container">
+              <InputField 
+                side='white-list-item-property-left' 
+                type='text' 
+                title={ADDRESS} 
+                value={this.state.crowdsale.whitelist.address} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'walletAddress')}/>
+              <InputField 
+                side='white-list-item-property-middle' 
+                type='number' 
+                title={MIN} 
+                value={this.state.crowdsale.whitelist.min} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'supply')}/>
+              <InputField 
+                side='white-list-item-property-right'
+                type='number' 
+                title={MAX} 
+                value={this.state.crowdsale.whitelist.max} 
+                onChange={(e) => this.changeState(e, 'crowdsale', 'supply')}/>
+            </div>
+          </div>
+          <div className="button-container">
+            {this.renderLinkComponent()}
+          </div>
+        </section>
+      )
+    }
+  }
 }
