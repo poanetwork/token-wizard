c05959a99b5c3596c1586dffff4a37715a7b3855
viktor <baranov.viktor.27@gmail.com>
2017-09-08 14:32:21 +0300

    Reserved tokens block, whitelist block equal widths for inputs

--- src/components/Common/ReservedTokensInputBlock.js
+++ src/components/Common/ReservedTokensInputBlock.js
@@ -59,32 +59,35 @@ export class ReservedTokensInputBlock extends React.Component {
         return (
             <div className="reserved-tokens-container">
               <div className="reserved-tokens-input-container">
-                  <InputField 
-                    side='reserved-tokens-input-property-left' 
-                    type='text' 
-                    title={ADDRESS} 
-                    onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_addr')}
-                    description={`Address where to send reserved tokens.`}
-                  />
-                  <RadioInputField 
-                    side='reserved-tokens-input-property-middle' 
-                    title={DIMENSION} 
-                    items={["tokens", "percentage"]}
-                    vals={["tokens", "percentage"]}
-                    defaultValue={this.state.token.reservedTokensInput.dim}
-                    name={'reserved-tokens-dim'}
-                    onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_dim')}
-                    description={`Fixed amount or % of crowdsaled tokens. Will be deposited to the account after fintalization of the crowdsale. `}
-                  />
-                  <InputField 
-                    side='reserved-tokens-input-property-right'
-                    type='number' 
-                    title={VALUE} 
-                    value={token.reservedTokensInput.val}
-                    onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_val')}
-                    description={`Value in tokens or percents. Don't forget to press + button for each reserved token.`}
-                  />
-              <div className="plus-button-container"><div onClick={(e) => this.addReservedTokensItem(token.reservedTokensInput.addr, token.reservedTokensInput.dim, token.reservedTokensInput.val, token)} className="button button_fill button_fill_plus"></div></div>
+                    <div className="reserved-tokens-input-container-inner">
+                          <InputField 
+                            side='reserved-tokens-input-property reserved-tokens-input-property-left' 
+                            type='text' 
+                            title={ADDRESS} 
+                            onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_addr')}
+                            description={`Address where to send reserved tokens.`}
+                          />
+                          <RadioInputField 
+                            side='reserved-tokens-input-property reserved-tokens-input-property-middle' 
+                            title={DIMENSION} 
+                            items={["tokens", "percentage"]}
+                            vals={["tokens", "percentage"]}
+                            defaultValue={this.state.token.reservedTokensInput.dim}
+                            name={'reserved-tokens-dim'}
+                            onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_dim')}
+                            description={`Fixed amount or % of crowdsaled tokens. Will be deposited to the account after fintalization of the crowdsale. `}
+                          />
+                          <InputField 
+                            side='reserved-tokens-input-property reserved-tokens-input-property-right'
+                            type='number' 
+                            title={VALUE} 
+                            value={token.reservedTokensInput.val}
+                            onChange={(e) => onChange(e, 'token', 0, 'reservedtokens_val')}
+                            description={`Value in tokens or percents. Don't forget to press + button for each reserved token.`}
+                          />
+                    </div>
+                    <div className="plus-button-container"><div onClick={(e) => this.addReservedTokensItem(token.reservedTokensInput.addr, token.reservedTokensInput.dim, token.reservedTokensInput.val, token)} className="button button_fill button_fill_plus"></div>
+                </div>
             </div>
             {token.reservedTokensElements}
             </div>)
--- src/components/Common/WhitelistInputBlock.js
+++ src/components/Common/WhitelistInputBlock.js
@@ -46,32 +46,38 @@ export class WhitelistInputBlock extends React.Component {
     render() {
         let { crowdsale } = this.state
         const { num } = this.props
-        return (<div className="white-list-container"><div className="white-list-input-container">
-              <InputField 
-                side='white-list-input-property-left' 
-                type='text' 
-                title={ADDRESS} 
-                value={crowdsale[num].whiteListInput.addr} 
-                onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_addr')}
-                description={`Address of a whitelisted account. Whitelists are inherited. E.g., if an account whitelisted on Tier 1 and didn't buy max cap on Tier 1, he can buy on Tier 2, and following tiers.`}
-              />
-              <InputField 
-                side='white-list-input-property-middle' 
-                type='number' 
-                title={MIN} 
-                value={crowdsale[num].whiteListInput.min} 
-                onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_min')}
-                description={`Minimum amount tokens to buy. Not a mininal size of a transaction. If minCap is 1 and user bought 1 token in a previous transaction and buying 0.1 token it will allow him to buy.`}
-              />
-              <InputField 
-                side='white-list-input-property-right'
-                type='number' 
-                title={MAX} 
-                value={crowdsale[num].whiteListInput.max} 
-                onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_max')}
-                description={`Maximum is the hard limit.`}
-              />
-              <div className="plus-button-container"><div onClick={(e) => this.addWhitelistItem(crowdsale[num].whiteListInput.addr, crowdsale[num].whiteListInput.min, crowdsale[num].whiteListInput.max)} className="button button_fill button_fill_plus"></div></div>
+        return (
+            <div className="white-list-container">
+                <div className="white-list-input-container">
+                    <div className="white-list-input-container-inner">
+                      <InputField 
+                        side='white-list-input-property white-list-input-property-left' 
+                        type='text' 
+                        title={ADDRESS} 
+                        value={crowdsale[num].whiteListInput.addr} 
+                        onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_addr')}
+                        description={`Address of a whitelisted account. Whitelists are inherited. E.g., if an account whitelisted on Tier 1 and didn't buy max cap on Tier 1, he can buy on Tier 2, and following tiers.`}
+                      />
+                      <InputField 
+                        side='white-list-input-property white-list-input-property-middle' 
+                        type='number' 
+                        title={MIN} 
+                        value={crowdsale[num].whiteListInput.min} 
+                        onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_min')}
+                        description={`Minimum amount tokens to buy. Not a mininal size of a transaction. If minCap is 1 and user bought 1 token in a previous transaction and buying 0.1 token it will allow him to buy.`}
+                      />
+                      <InputField 
+                        side='white-list-input-property white-list-input-property-right'
+                        type='number' 
+                        title={MAX} 
+                        value={crowdsale[num].whiteListInput.max} 
+                        onChange={(e) => this.props.onChange(e, 'crowdsale', this.props.num, 'whitelist_max')}
+                        description={`Maximum is the hard limit.`}
+                      />
+                    </div>
+                    <div className="plus-button-container"><div onClick={(e) => this.addWhitelistItem(crowdsale[num].whiteListInput.addr, crowdsale[num].whiteListInput.min, crowdsale[num].whiteListInput.max)} className="button button_fill button_fill_plus">
+                    </div>
+                </div>
             </div>
             {crowdsale[num].whiteListElements}
             </div>)
