853d5edf400b359658c15f3b775d1e203a2254b7
viktor <baranov.viktor.27@gmail.com>
2017-09-11 15:10:46 +0300

    Radio inputs are styled; #109

--- src/components/stepOne.js
+++ src/components/stepOne.js
@@ -161,21 +161,6 @@ export class stepOne extends React.Component {
                 Modern crowdsale strategy with multiple tiers, whitelists, and limits. Recommended for every crowdsale. 
               </span>
             </label>
-            {/* <label className="radio">
-              <input 
-                type="radio" 
-                disabled 
-                checked={this.state.contractType === this.state.contractTypes.capped}    
-                name="contract-type"
-                id={this.state.contractTypes.whitelistwithcap}
-                onChange={(e) => this.contractTypeSelected(e)}
-              />
-              <span className="title title_soon">Capped</span>
-              <span className="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </span>
-            </label> */}
           </div>
         </div>
         <div className="button-container">
