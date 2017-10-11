db8ad2a9c3da8e6112bf6e93b3d45ab3a4549726
sdzharkov <stephan.zharkov@gmail.com>
2017-10-10 21:43:11 -0700

    Delete unnecessary comments

--- src/components/stepTwo/index.js
+++ src/components/stepTwo/index.js
@@ -2,35 +2,19 @@ import React, { Component } from 'react'
 import '../../assets/stylesheets/application.css';
 import { Link } from 'react-router-dom'
 import { checkWeb3 } from '../../utils/blockchainHelpers'
-import { getOldState, stepsAreValid, validateValue, allFieldsAreValid } from '../../utils/utils'
 import { StepNavigation } from '../Common/StepNavigation'
 import { InputField } from '../Common/InputField'
 import { ReservedTokensInputBlock } from '../Common/ReservedTokensInputBlock'
-import { NAVIGATION_STEPS, VALIDATION_MESSAGES, VALIDATION_TYPES, defaultState, TEXT_FIELDS, intitialStepTwoValidations } from '../../utils/constants'
+import { NAVIGATION_STEPS, VALIDATION_MESSAGES, TEXT_FIELDS } from '../../utils/constants'
 import { inject, observer } from 'mobx-react';
 const { TOKEN_SETUP } = NAVIGATION_STEPS
-const { EMPTY, VALID, INVALID } = VALIDATION_TYPES
 const { NAME, TICKER, DECIMALS } = TEXT_FIELDS
 
 @inject('tokenStore', 'web3Store', 'tierCrowdsaleListStore') @observer
 export class stepTwo extends Component {
-  constructor(props) {
-    super(props);
-    window.scrollTo(0, 0);
-    console.log('props', props)
-    let oldState = getOldState(props, defaultState)
-    console.log('oldState', oldState)
-    this.state = Object.assign({}, defaultState, oldState, intitialStepTwoValidations )
-  }
 
   componentDidMount() {
     checkWeb3(this.props.web3Store.web3);
-  }
-
-  getNewParent (property, parent, value) {
-    let newParent = { ...this.state[`${parent}`] }
-    newParent[property] = value
-    return newParent
   }
 
   showErrorMessages = (parent) => {
@@ -70,98 +54,19 @@ export class stepTwo extends Component {
     this.props.tokenStore.validateTokens(property);
   }
 
-  // changeInputField = (event, parent, key, property) => {
-  //   let value = event.target.value
-  //   console.log("parent: " + parent, "key: " + key, "property: " + property, "value: " + value);
-  //   let newState = { ...this.state }
-  //   console.log(newState);
-  //   if (property === "startTime" || property === "endTime") {
-  //     let targetTime = new Date(value);
-  //     let targetTimeTemp = targetTime.setHours(targetTime.getHours() - targetTime.getTimezoneOffset()/60);//.setUTCHours(new Date(targetTime).getHours());
-  //     if (property === "startTime") {
-  //       console.log("property == startTime");
-  //       if (targetTimeTemp)
-  //         newState.crowdsale[key].startTime = new Date(targetTimeTemp).toISOString().split(".")[0];
-  //       else
-  //         newState.crowdsale[key].startTime = null;
-  //     } else if (property === "endTime") {
-  //       if (targetTimeTemp)
-  //         newState.crowdsale[key].endTime = new Date(targetTimeTemp).toISOString().split(".")[0];
-  //       else 
-  //         newState.crowdsale[key].endTime = null
-  //       if (newState.crowdsale[key + 1]) {
-  //         newState.crowdsale[key + 1].startTime = newState.crowdsale[key].endTime;
-  //         let newEndDate = new Date(newState.crowdsale[key].endTime);
-  //         newEndDate = newEndDate.setDate(new Date(newState.crowdsale[key].endTime).getDate() + 4);;
-  //         newState.crowdsale[key + 1].endTime = new Date(newEndDate).toISOString().split(".")[0];
-  //       }
-  //     }
-  //     //depreciated
-  //     //this.setBlockTimes(key, property, targetTime)
-  //   } else if (property.indexOf("whitelist_") === 0) {
-  //     let prop = property.split("_")[1];
-  //     newState.crowdsale[key][`whiteListInput`][prop] = value
-  //   } else if (property.indexOf("reservedtokens_") === 0) {
-  //     console.log(newState);
-  //     let prop = property.split("_")[1];
-  //     newState.token[`reservedTokensInput`][prop] = value
-  //   } else {
-  //     if( Object.prototype.toString.call( newState[parent] ) === '[object Array]' ) {
-  //       newState[parent][key][property] = value;//this.getNewParent(property, parent, key, value)
-  //     } else {
-  //       newState[parent][property] = value;//this.getNewParent(property, parent, key, value)
-  //     }
-  //   }
-  //   if (property.indexOf("whitelist") === -1 && property.indexOf("reservedtokens") === -1) {
-
-  //     if ( Object.prototype.toString.call( newState[`validations`] ) === '[object Array]' ) {
-  //       newState[`validations`][key][property] = validateValue(value, property, newState)
-  //     } else {
-  //       newState[`validations`][property] = validateValue(value, property, newState)
-  //     }
-  //     //console.log('property', property)
-  //     //console.log('newState[`validations`][property]',  newState[`validations`], validateValue(value, property, newState), 'newState', newState)
-  //   }
-  //   console.log('newState', newState)
-  //   this.setState(newState)
-  // }
-
-  handleInputBlur (parent, property, key) {
-    //console.log(parent, property, key);
-    let newState = { ...this.state }
-    let value
-    if (property === 'rate') {
-      value = newState[parent][key][property]
-    } else {
-      value = key === undefined ? newState[parent][property] : newState[parent][key][property]
-    }
-
-    if ( Object.prototype.toString.call( newState[`validations`] ) === '[object Array]' ) {
-      if (!key) {
-        newState[`validations`][property] = validateValue(value, property, newState)
-      } else {
-        newState[`validations`][key][property] = validateValue(value, property, newState)
-      }
-    } else {
-      newState[`validations`][property] = validateValue(value, property, newState)
-    }
-    this.setState(newState)
-  }
-
   renderLink () {
     return <Link className="button button_fill" to='/3'>Continue</Link>
   }
 
   renderLinkComponent = () => {
     if(this.props.tokenStore.isTokenValid) {
-      return this.renderLink()
+      return this.renderLink();
     }
+
     return <div onClick={this.showErrorMessages.bind(this, 'token')} className="button button_fill"> Continue</div>
   }
 
   render() {
-    const { token, validations } = this.state
-    console.log('step 2', this.state)
     return (
     	<section className="steps steps_crowdsale-contract" ref="two">
         <StepNavigation activeStep={TOKEN_SETUP}/>
@@ -179,7 +84,6 @@ export class stepTwo extends Component {
               valid={this.props.tokenStore.validToken['name']} 
               title={NAME} 
               value={this.props.tokenStore.name} 
-              onBlur={() => this.handleInputBlur('token', 'name')}
               onChange={(e) => this.updateTokenStore(e, 'name')}
               description={`The name of your token. Will be used by Etherscan and other token browsers. Be afraid of trademarks.`}
             />
@@ -189,7 +93,6 @@ export class stepTwo extends Component {
               valid={this.props.tokenStore.validToken['ticker']} 
               title={TICKER} 
               value={this.props.tokenStore.ticker} 
-              onBlur={() => this.handleInputBlur('token', 'ticker')}
               onChange={(e) => this.updateTokenStore(e, 'ticker')}
               description={`The three letter ticker for your token. There are 17,576 combinations for 26 english letters. Be hurry. `}
             />
@@ -199,7 +102,6 @@ export class stepTwo extends Component {
               valid={this.props.tokenStore.validToken['decimals']} 
               title={DECIMALS}
               value={this.props.tokenStore.decimals} 
-              onBlur={() => this.handleInputBlur('token', 'decimals')}
               onChange={(e) => this.updateTokenStore(e, 'decimals')} // changeInputField
               description={`Refers to how divisible a token can be, from 0 (not at all divisible) to 18 (pretty much continuous).`}
             />
