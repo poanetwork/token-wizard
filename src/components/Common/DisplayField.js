db335c69f68668c543899623ad8e8bc8b3e19579
Franco Victorio <victorio.franco@gmail.com>
2017-11-29 14:18:46 -0300

    Make DisplayField pure

--- src/components/Common/DisplayField.js
+++ src/components/Common/DisplayField.js
@@ -1,24 +1,17 @@
-import React from 'react'
+import React from 'react';
 import '../../assets/stylesheets/application.css';
 
-export class DisplayField extends React.Component {
-	constructor(props) {
-    	super(props);
-	}
-
-	render() {
-		return (<div className={this.props.side}>
-			<div className="display-container">
-	        	<p className="label">{this.props.title}</p>
-	        	<p className="value">{this.props.value}</p>
-		        <p className="description">
-		          {this.props.description}
-		        </p>
-	        </div>
-	        <div className="copy-field-container">
-	        	<btn className="copy" data-clipboard-action="copy" data-clipboard-text={this.props.value}></btn>
-	        </div>
-	      </div>
-	    )
-  	}
-}
+export const DisplayField = props => {
+  return (
+    <div className={props.side}>
+      <div className="display-container">
+        <p className="label">{props.title}</p>
+        <p className="value">{props.value}</p>
+        <p className="description">{props.description}</p>
+      </div>
+      <div className="copy-field-container">
+        <btn className="copy" data-clipboard-action="copy" data-clipboard-text={props.value} />
+      </div>
+    </div>
+  );
+};
