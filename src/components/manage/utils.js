a79ee2ae61980c8b687d4129be4d29bf57aa163c
fernandomg <fernando.greco@gmail.com>
2018-01-09 15:45:10 -0300

    Remove commented code

--- src/components/manage/utils.js
+++ src/components/manage/utils.js
@@ -95,18 +95,8 @@ export const updateTierAttribute = (attribute, value, addresses) => {
 const extractWhitelistInformation = (isWhitelisted, crowdsaleMethods) => {
   let whitelistedAccounts = []
   const whenWhitelistedAddresses = []
-  // const whitelistedAddresses = [
-  //   '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1',
-  //   '0xffcf8fdee72ac11b5c542428b35eef5769c409f0',
-  //   '0x22d491bde2303f2f43325b2108d26f1eaba1e32b',
-  //   '0xe11ba2b4d45eaed5996cd0823791e0c93114882d',
-  //   '0xd03ea8624c8c5987235048901fb614fdca89b117',
-  //   '0x95ced938f7991cd0dfcb48f0a06a40fa1af46ebc'
-  // ]
-  // const participantsCount = whitelistedAddresses.length
 
   if (isWhitelisted && crowdsaleMethods.whitelistedParticipantsLength) {
-  // if (isWhitelisted) {
     return crowdsaleMethods.whitelistedParticipantsLength().call()
       .then(participantsCount => {
         for (let participantIndex = 0; participantIndex < participantsCount; participantIndex++) {
@@ -125,7 +115,6 @@ const extractWhitelistInformation = (isWhitelisted, crowdsaleMethods) => {
   }
 
   return Promise.resolve([isWhitelisted, whenWhitelistedAddresses, whitelistedAccounts])
-  // return Promise.resolve([isWhitelisted, whitelistedAddresses, whitelistedAccounts])
 }
 
 const crowdsaleData = crowdsaleAddress => {
