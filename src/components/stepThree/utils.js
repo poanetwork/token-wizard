02de7437b658e08402a2b6805944ded1fd9050b2
Victor Baranov <baranov.viktor.27@gmail.com>
2018-01-15 17:31:34 +0300

    Remove now variable
     to avoid possible confusions due to object mutability

--- src/components/stepThree/utils.js
+++ src/components/stepThree/utils.js
@@ -1,8 +1,7 @@
 import moment from 'moment';
 
 export function defaultCompanyStartDate() {
-  let now = moment();
-  let crowdsaleStartDate = now.add(5, 'minutes');
+  let crowdsaleStartDate = moment().add(5, 'minutes');
   let crowdsaleStartDateFormatted = crowdsaleStartDate.format('YYYY-DD-MMTHH:mm');
   return crowdsaleStartDateFormatted;
 }
