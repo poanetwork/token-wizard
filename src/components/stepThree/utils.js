cdb258a6ac3f77d90fe601108ff1b83bee14aa6b
viktor <baranov.viktor.27@gmail.com>
2018-01-16 12:11:55 +0300

    (Emergency) Default start time fix: Closes #493, Closes #494

--- src/components/stepThree/utils.js
+++ src/components/stepThree/utils.js
@@ -2,6 +2,6 @@ import moment from 'moment';
 
 export function defaultCompanyStartDate() {
   let crowdsaleStartDate = moment().add(5, 'minutes');
-  let crowdsaleStartDateFormatted = crowdsaleStartDate.format('YYYY-DD-MMTHH:mm');
+  let crowdsaleStartDateFormatted = crowdsaleStartDate.format('YYYY-MM-DDTHH:mm');
   return crowdsaleStartDateFormatted;
 }
