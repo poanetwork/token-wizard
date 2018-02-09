923ec771aee20a877892b4dc0b2f8769db4129fd
Franco Victorio <victorio.franco@gmail.com>
2018-02-09 15:25:48 -0300

    Check if there is an unfinished deploy

---
+++ src/components/IncompleteDeploy.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class CheckIncompleteDeploy extends Component {
+  cancel() {
+    localStorage.clear()
+    window.location.reload()
+  }
+
+  render() {
+    return (
+      <div>
+        <section className="home">
+          <div className="crowdsale">
+            <div className="container">
+              <h1 className="title">Welcome to Token Wizard</h1>
+              <p className="description">
+                You have an incomplete deploy.
+              </p>
+              <div className="buttons">
+                <Link to='/4'><span className="button button_fill">Resume</span></Link>
+                <div onClick={this.cancel} className="button button_outline">Cancel</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default CheckIncompleteDeploy
