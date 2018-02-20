814801f419a59bad3688f32bc3ee816a63bdc6ca
fernandomg <fernando.greco@gmail.com>
2018-02-20 11:42:51 -0300

    Update NumericInput tests

--- src/components/Common/NumericInput.spec.js
+++ src/components/Common/NumericInput.spec.js
@@ -20,6 +20,10 @@ let mock
 let keypressMock
 let pasteMock
 let numericInputComponent
+let wrapperMemo
+let wrapper
+let inputMemo
+let input
 
 configure({ adapter: new Adapter() })
 
@@ -43,222 +47,271 @@ beforeEach(() => {
     errorMessage: VALIDATION_MESSAGES.DECIMALS,
     onValueUpdate: jest.fn()
   }
+
+  wrapperMemo = undefined
+  wrapper = () => wrapperMemo || (wrapperMemo = mount(React.createElement(NumericInput, numericInputComponent)))
+
+  inputMemo = undefined
+  input = () => inputMemo || (inputMemo = wrapper().find('input').at(0))
 })
 
 describe('NumericInput', () => {
   it('Should render the component', () => {
-    numericInputComponent.min = '0'
-    numericInputComponent.max = '18'
+    numericInputComponent.min = 0
+    numericInputComponent.max = 18
     numericInputComponent.acceptFloat = true
-    numericInputComponent.minDecimals = '0'
-    numericInputComponent.maxDecimals = '4'
+    numericInputComponent.minDecimals = 0
+    numericInputComponent.maxDecimals = 4
 
-    const component = renderer.create(React.createElement(NumericInput, numericInputComponent))
-    const tree = component.toJSON()
-
-    expect(tree).toMatchSnapshot()
+    expect(renderer.create(React.createElement(NumericInput, numericInputComponent)).toJSON()).toMatchSnapshot()
   })
 
-  it('Should validate min', () => {
+  it('Should fail if value is less than min', () => {
     numericInputComponent.min = 5
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
     mock.target.value = '4'
-    const expectedInvalidValue = parseFloat(mock.target.value)
 
-    input.simulate(INPUT_EVENT.CHANGE, mock)
-    expect(wrapper.state(COMPONENT_STATE.VALUE)).toBe(expectedInvalidValue)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(parseFloat(mock.target.value))
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+  })
 
+  it('Should pass if value is greater than min', () => {
+    numericInputComponent.min = 5
     mock.target.value = '8'
-    const expectedValidValue = parseFloat(mock.target.value)
 
-    input.simulate(INPUT_EVENT.CHANGE, mock)
-    expect(wrapper.state(COMPONENT_STATE.VALUE)).toBe(expectedValidValue)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(parseFloat(mock.target.value))
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
   })
 
   it('Should validate max', () => {
     numericInputComponent.max = 15
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
     mock.target.value = '20'
-    const expectedInvalidValue = parseFloat(mock.target.value)
 
-    input.simulate(INPUT_EVENT.CHANGE, mock)
-    expect(wrapper.state(COMPONENT_STATE.VALUE)).toBe(expectedInvalidValue)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(parseFloat(mock.target.value))
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+  })
 
+  it('Should validate max', () => {
+    numericInputComponent.max = 15
     mock.target.value = '10'
-    const expectedValidValue = parseFloat(mock.target.value)
 
-    input.simulate(INPUT_EVENT.CHANGE, mock)
-    expect(wrapper.state(COMPONENT_STATE.VALUE)).toBe(expectedValidValue)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(parseFloat(mock.target.value))
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
   })
 
   it('Should consider empty string as valid', () => {
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
-    input.simulate(INPUT_EVENT.CHANGE, mock)
+    input().simulate(INPUT_EVENT.CHANGE, mock)
     expect(numericInputComponent.onValueUpdate).toHaveBeenCalled()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+  })
+
+  it('Should accept "-" symbol if min is negative', () => {
+    numericInputComponent.min = -10
+    keypressMock.key = '-'
+
+    input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+    expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
+  })
+
+  it('Should accept "-" symbol if max is negative', () => {
+    numericInputComponent.max = -10
+    keypressMock.key = '-'
+
+    input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+    expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
+  })
+
+  it('Should reject "-" symbol if min is not negative', () => {
+    numericInputComponent.min = 0
+    keypressMock.key = '-'
+
+    input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+    expect(keypressMock.preventDefault).toHaveBeenCalled()
+  })
+
+  it('Should reject "-" symbol if max is not negative', () => {
+    numericInputComponent.max = 10
+    keypressMock.key = '-'
+
+    input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+    expect(keypressMock.preventDefault).toHaveBeenCalled()
+  })
+
+  it('Should reject "-" symbol if it is not the first character', () => {
+    numericInputComponent.min = -10
+    mock.target.value = 5
+    keypressMock.key = '-'
+
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(5)
+
+    input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+    expect(keypressMock.preventDefault).toHaveBeenCalled()
   })
 
   it('Should prevent pasting invalid value', () => {
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
-    input.simulate(INPUT_EVENT.PASTE, pasteMock)
+    input().simulate(INPUT_EVENT.PASTE, pasteMock)
     expect(pasteMock.preventDefault).toHaveBeenCalled()
   })
 
   it('Should allow pasting valid value', () => {
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
     pasteMock.clipboardData.getData = () => '12'
 
-    input.simulate(INPUT_EVENT.PASTE, pasteMock)
+    input().simulate(INPUT_EVENT.PASTE, pasteMock)
     expect(pasteMock.preventDefault).toHaveBeenCalledTimes(0)
   })
 
   it('Should call onValueUpdate callback on successful update', () => {
-    const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-    const input = wrapper.find('input').at(0)
-
     mock.target.value = '10'
     const expectedValue = parseFloat(mock.target.value)
 
-    input.simulate(INPUT_EVENT.CHANGE, mock)
-    expect(wrapper.state(COMPONENT_STATE.VALUE)).toBe(expectedValue)
-    expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-    expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
-
+    input().simulate(INPUT_EVENT.CHANGE, mock)
+    expect(wrapper().state(COMPONENT_STATE.VALUE)).toBe(expectedValue)
+    expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+    expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
     expect(numericInputComponent.onValueUpdate).toHaveBeenCalledTimes(1)
     expect(numericInputComponent.onValueUpdate).toHaveBeenCalledWith(expectedValue)
   })
 
   describe('float numbers', () => {
-    it ('Should validate maxDecimals', () => {
+    beforeEach(() => {
       numericInputComponent.acceptFloat = true
-      numericInputComponent.maxDecimals = '4'
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
-
-      mock.target.value = '1.12345'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
-
-      mock.target.value = '1.1234'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+      numericInputComponent.minDecimals = 2
+      numericInputComponent.maxDecimals = 4
     })
 
-    it ('Should validate minDecimals', () => {
-      numericInputComponent.acceptFloat = true
-      numericInputComponent.minDecimals = '2'
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
+    it('Should fail if value has more decimals than maxDecimals', () => {
+      mock.target.value = '1.12345'
 
-      mock.target.value = '1.1'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
-
-      mock.target.value = '1.1234'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
     })
 
-    it ('Should validate minDecimals and maxDecimals', () => {
-      numericInputComponent.acceptFloat = true
-      numericInputComponent.maxDecimals = '4'
-      numericInputComponent.minDecimals = '2'
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
-
-      mock.target.value = '1.1'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
-
-      mock.target.value = '1.12345'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
-
-      mock.target.value = '1.12'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
-
+    it('Should pass if value has less decimals than maxDecimals', () => {
       mock.target.value = '1.123'
-      input.simulate(INPUT_EVENT.CHANGE, mock)
-      expect(wrapper.state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
-      expect(wrapper.state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    })
+
+    it('Should pass if value has same decimals than maxDecimals', () => {
+      mock.target.value = '1.1234'
+
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    })
+
+    it('Should fail if value has less decimals than minDecimals', () => {
+      mock.target.value = '1.1'
+
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+    })
+
+    it('Should pass if value has more decimals than minDecimals', () => {
+      mock.target.value = '1.1234'
+
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    })
+
+    it('Should pass if value has same decimals than minDecimals', () => {
+      mock.target.value = '1.12'
+
+      input().simulate(INPUT_EVENT.CHANGE, mock)
+      expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+      expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+    })
+
+    describe('range of [minDecimals, maxDecimals]', () => {
+      it('Should fail for: 1.1, if it is outside the range', () => {
+        mock.target.value = '1.1'
+
+        input().simulate(INPUT_EVENT.CHANGE, mock)
+        expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+        expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+      })
+
+      it('Should fail for: 1.12345, if it is outside the range', () => {
+        mock.target.value = '1.12345'
+
+        input().simulate(INPUT_EVENT.CHANGE, mock)
+        expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+        expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.INVALID)
+      })
+
+      it('Should pass for: 1.12, if it is inside the range', () => {
+        mock.target.value = '1.12'
+
+        input().simulate(INPUT_EVENT.CHANGE, mock)
+        expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+        expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+      })
+
+      it('Should pass for: 1.123, if it is inside the range', () => {
+        mock.target.value = '1.123'
+
+        input().simulate(INPUT_EVENT.CHANGE, mock)
+        expect(wrapper().state(COMPONENT_STATE.PRISTINE)).toBeFalsy()
+        expect(wrapper().state(COMPONENT_STATE.VALID)).toBe(VALIDATION_TYPES.VALID)
+      })
     })
 
     it('Should reject float with dot notation', () => {
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
+      numericInputComponent.acceptFloat = false
 
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
 
       keypressMock.key = '.'
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalled()
     })
 
     it('Should accept float with dot notation', () => {
-      numericInputComponent.acceptFloat = true
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
-
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
 
       keypressMock.key = '.'
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
     })
 
     it('Should reject float with scientific notation', () => {
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
+      numericInputComponent.acceptFloat = false
 
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
 
       keypressMock.key = 'e'
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalled()
     })
 
     it('Should accept float with scientific notation', () => {
-      numericInputComponent.acceptFloat = true
-      const wrapper = mount(React.createElement(NumericInput, numericInputComponent))
-      const input = wrapper.find('input').at(0)
-
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
 
       keypressMock.key = 'e'
-      input.simulate(INPUT_EVENT.KEYPRESS, keypressMock)
+
+      input().simulate(INPUT_EVENT.KEYPRESS, keypressMock)
       expect(keypressMock.preventDefault).toHaveBeenCalledTimes(0)
     })
-
   })
 })
