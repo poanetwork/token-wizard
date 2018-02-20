c489e693d98180c9323d01182b06e78fa647f47a
fernandomg <fernando.greco@gmail.com>
2018-02-20 16:44:53 -0300

    Update validateMinCap method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -98,7 +98,7 @@ export const validateLaterOrEqualTime = (laterTime, previousTime) => getTimeAsNu
 
 export const validateRate = (rate) => isNaN(Number(rate)) === false && Number(rate) > 0
 
-export const validateMinCap = minCap => !isNaN(Number(minCap)) && Number(minCap) > 0
+export const validateMinCap = minCap => /^$|^([0-9]+)$/.test(minCap)
 
 export const validateAddress = (address) => {
   if(!address || address.length !== 42 ) {
