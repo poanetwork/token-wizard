d57eb92d1284ff76568bbd3ecbe1674da8ed523a
fernandomg <fernando.greco@gmail.com>
2018-03-06 09:31:45 -0300

    Change attribute name

--- src/components/manage/index.js
+++ src/components/manage/index.js
@@ -514,7 +514,7 @@ export class Manage extends Component {
           side='left'
           type='text'
           title={CROWDSALE_SETUP_NAME}
-          value={tier.name}
+          value={tier.tier}
           disabled={true}
         />
         <InputField
