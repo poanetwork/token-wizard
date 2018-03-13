fa169a660a0d73a30b4a7a07ccbf6e9af94e32d8
Franco Victorio <victorio.franco@gmail.com>
2018-03-13 16:03:45 -0300

    Move deploy cancellation to its own file

---
+++ src/utils/cancelDeploy.js
@@ -0,0 +1,13 @@
+import { cancellingIncompleteDeploy } from './alerts'
+
+const cancelDeploy = () => {
+  return cancellingIncompleteDeploy()
+    .then(result => {
+      if (result.value) {
+        localStorage.clear()
+        window.location = '/' // go to home
+      }
+    })
+}
+
+export default cancelDeploy
