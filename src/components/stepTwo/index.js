4a41d1a0297232d518f0c9d5d2d724ff8699e4da
fernandomg <fernando.greco@gmail.com>
2018-03-21 16:07:20 -0300

    Update stepTwo component to use final-form

--- src/components/stepTwo/index.js
+++ src/components/stepTwo/index.js
@@ -1,115 +1,48 @@
 import React, { Component } from 'react'
-import '../../assets/stylesheets/application.css';
+import '../../assets/stylesheets/application.css'
 import { Link } from 'react-router-dom'
 import { checkWeb3 } from '../../utils/blockchainHelpers'
 import { StepNavigation } from '../Common/StepNavigation'
-import { InputField } from '../Common/InputField'
-import { NumericInput } from '../Common/NumericInput'
 import { ReservedTokensInputBlock } from '../Common/ReservedTokensInputBlock'
 import { clearingReservedTokens } from '../../utils/alerts'
-import {
-  NAVIGATION_STEPS,
-  VALIDATION_MESSAGES,
-  TEXT_FIELDS,
-  DESCRIPTION,
-  VALIDATION_TYPES
-} from '../../utils/constants'
-import { inject, observer } from 'mobx-react';
-import update from 'immutability-helper'
+import { NAVIGATION_STEPS, VALIDATION_TYPES } from '../../utils/constants'
+import { inject, observer } from 'mobx-react'
+import { Form } from 'react-final-form'
+import { validators } from '../../utils/validations'
+import { StepTwoForm } from './StepTwoForm'
 
 const { TOKEN_SETUP } = NAVIGATION_STEPS
-const { NAME, TICKER, DECIMALS } = TEXT_FIELDS
 const { VALID, INVALID } = VALIDATION_TYPES
 
 @inject('tokenStore', 'web3Store', 'reservedTokenStore')
 @observer
 export class stepTwo extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
-      decimals: '',
-      validation: {
-        decimals: {
-          disabled: false,
-          pristine: true,
-          valid: INVALID
-        }
+      tokenValues: {
+        name: this.props.tokenStore.name,
+        ticker: this.props.tokenStore.ticker,
+        decimals: this.props.tokenStore.decimals,
       }
     }
   }
 
   componentDidMount() {
-    checkWeb3(this.props.web3Store.web3);
+    checkWeb3(this.props.web3Store.web3)
   }
 
-  showErrorMessages = (parent) => {
-    this.props.tokenStore.invalidateToken();
-    this.updateDecimalsStore({
-      value: this.state.decimals,
-      pristine: false,
-      valid: this.state.validation.decimals.valid
-    })
-  }
+  beforeNavigate = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
 
-  updateTokenStore = (event, property) => {
-    const value = event.target.value;
-    this.props.tokenStore.setProperty(property, value);
-    this.props.tokenStore.validateTokens(property);
-  }
-
-  updateDecimalsStore = ({ value, pristine, valid }) => {
-    const newState = update(this.state, {
-      validation: {
-        decimals: {
-          $set: {
-            disabled: this.state.validation.decimals.disabled,
-            pristine: pristine,
-            valid: valid
-          },
-        },
-      },
-    })
-    newState.decimals = value
-
-    this.setState(newState)
-
-    // TODO: store should only be updated when the user hits 'Continue'
-    const { tokenStore } = this.props
-    tokenStore.setProperty('decimals', value)
-    tokenStore.updateValidity('decimals', valid)
-  }
-
-  disableDecimals = () => {
-    if (this.state.decimals === '') {
-      this.updateDecimalsStore({ value: 0, pristine: false, valid: VALID })
-    }
-
-    this.setState(update(this.state, { validation: { decimals: { disabled: { $set: true } } } }))
-  }
-
-  enableDecimals = () => {
-    this.setState(update(this.state, { validation: { decimals: { disabled: { $set: false } } } }))
-  }
-
-  renderLink () {
-    return <Link className="button button_fill" to='/3'>Continue</Link>
-  }
-
-  renderLinkComponent = () => {
-    if(this.props.tokenStore.isTokenValid) {
-      return this.renderLink();
-    }
-    return <div onClick={this.showErrorMessages.bind(this, 'token')} className="button button_fill"> Continue</div>
+    document
+      .getElementById('tokenData')
+      .dispatchEvent(new Event('submit'))
   }
 
   removeReservedToken = index => {
-    const lastReservedItem = this.props.reservedTokenStore.tokens.length === 1
-
-    if (lastReservedItem) {
-      this.enableDecimals()
-    }
-
     this.props.reservedTokenStore.removeToken(index)
   }
 
@@ -117,26 +50,29 @@ export class stepTwo extends Component {
     return clearingReservedTokens()
       .then(result => {
         if (result.value) {
-          this.enableDecimals()
-
           this.props.reservedTokenStore.clearAll()
         }
       })
   }
 
   addReservedTokensItem = newToken => {
-    const firstReservedItem = !this.props.reservedTokenStore.tokens.length
-
-    if (firstReservedItem) {
-      this.disableDecimals()
-    }
-
     this.props.reservedTokenStore.addToken(newToken)
   }
 
+  updateTokenStore = (property, value) => {
+    const validity = validators(property, value) ? VALID : INVALID
+
+    this.props.tokenStore.setProperty(property, value)
+    this.props.tokenStore.updateValidity(property, validity)
+  }
+
+  onSubmit = () => {
+    this.props.history.push('/3')
+  }
+
   render () {
-    const decimals = this.state.validation.decimals.valid === VALID && this.state.decimals >= 0
-      ? parseInt(this.state.decimals, 10)
+    const decimals = this.props.tokenStore.validToken.decimals === VALID && this.props.tokenStore.decimals >= 0
+      ? parseInt(this.props.tokenStore.decimals, 10)
       : 0
 
     return (
@@ -150,38 +86,14 @@ export class stepTwo extends Component {
               Configure properties of your token. Created token contract will be ERC-20 compatible.
             </p>
           </div>
-          <div className="hidden">
-            <InputField side='left' type='text'
-              errorMessage={VALIDATION_MESSAGES.NAME}
-              valid={this.props.tokenStore.validToken['name']}
-              title={NAME}
-              value={this.props.tokenStore.name}
-              onChange={(e) => this.updateTokenStore(e, 'name')}
-              description={`The name of your token. Will be used by Etherscan and other token browsers. Be afraid of trademarks.`}
-            />
-            <InputField
-              side='right' type='text'
-              errorMessage={VALIDATION_MESSAGES.TICKER}
-              valid={this.props.tokenStore.validToken['ticker']}
-              title={TICKER}
-              value={this.props.tokenStore.ticker}
-              onChange={(e) => this.updateTokenStore(e, 'ticker')}
-              description={`${DESCRIPTION.TOKEN_TICKER} There are 11,881,376 combinations for 26 english letters. Be hurry. `}
-            />
-            <NumericInput
-              disabled={this.state.validation.decimals.disabled}
-              side="left"
-              title={DECIMALS}
-              description="Refers to how divisible a token can be, from 0 (not at all divisible) to 18 (pretty much continuous)."
-              min={0}
-              max={18}
-              value={this.state.decimals}
-              pristine={this.state.validation.decimals.pristine}
-              valid={this.state.validation.decimals.valid}
-              errorMessage={VALIDATION_MESSAGES.DECIMALS}
-              onValueUpdate={this.updateDecimalsStore}
-            />
-          </div>
+          <Form
+            onSubmit={this.onSubmit}
+            initialValues={this.state.tokenValues}
+            component={StepTwoForm}
+            disableDecimals={!!this.props.reservedTokenStore.tokens.length}
+            updateTokenStore={this.updateTokenStore}
+            id="tokenData"
+          />
           <div className="reserved-tokens-title">
             <p className="title">Reserved tokens</p>
           </div>
@@ -194,7 +106,7 @@ export class stepTwo extends Component {
           />
         </div>
         <div className="button-container">
-          {this.renderLinkComponent()}
+          <Link onClick={e => this.beforeNavigate(e)} className="button button_fill" to="/3">Continue</Link>
         </div>
       </section>
     )}
