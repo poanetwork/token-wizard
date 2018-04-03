975db02b2576854934bcfddec511b9e301157072
fernandomg <fernando.greco@gmail.com>
2018-04-03 14:26:04 -0300

    Modify InputField2 to use Error component

--- src/components/Common/InputField2.js
+++ src/components/Common/InputField2.js
@@ -1,24 +1,19 @@
 import React from 'react'
+import { Error } from './Error'
 
-export const InputField2 = ({ input, meta, side, label, type, description, disabled, errorStyle }) => {
-  const errors = [].concat(meta.error)
-
-  return (
-    <div className={side}>
-      <label htmlFor={input.name} className="label">{label}</label>
-      <input
-        autoComplete="off"
-        className="input"
-        type={type}
-        disabled={disabled}
-        id={input.name}
-        {...input}
-      />
-      <p className="description">{description}</p>
-      {errors.map((error, index)=> (
-        <p key={index} style={errorStyle}>{(!meta.pristine || meta.touched) && error}</p>
-      ))}
-    </div>
-  )
-}
+export const InputField2 = (props) => (
+  <div className={props.side}>
+    <label htmlFor={props.input.name} className="label">{props.label}</label>
+    <input
+      autoComplete="off"
+      className="input"
+      type={props.type}
+      disabled={props.disabled}
+      id={props.input.name}
+      {...props.input}
+    />
+    <p className="description">{props.description}</p>
+    <Error name={props.input.name} errorStyle={props.errorStyle}/>
+  </div>
+)
 
