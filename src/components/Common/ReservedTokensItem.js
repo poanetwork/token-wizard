bf2dcfc2c4fa3495342f39d6f81a5c4be6b978e8
fernandomg <fernando.greco@gmail.com>
2018-04-26 16:52:35 -0300

    Update ReservedTokensItem component and tests

--- src/components/Common/ReservedTokensItem.js
+++ src/components/Common/ReservedTokensItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import '../../assets/stylesheets/application.css';
+import classNames from 'classnames'
 
 const ReservedTokensItem = props => {
   return (
-    <div className={'reserved-tokens-item-container'}>
-      <div className="reserved-tokens-item-container-inner">
+    <div className="reserved-tokens-item-container">
+      <div className={classNames('reserved-tokens-item-container-inner', { 'monospace': props.readOnly })}>
         <span className="reserved-tokens-item reserved-tokens-item-left">{props.addr}</span>
         <span className="reserved-tokens-item reserved-tokens-item-middle">{props.dim}</span>
         <span className="reserved-tokens-item reserved-tokens-item-right">{props.val}</span>
       </div>
-      <div className="reserved-tokens-item-empty">
-        <a onClick={() => props.onRemove(props.num)}>
-          <span className="item-remove" />
-        </a>
-      </div>
+      {props.readOnly
+        ? null
+        : <div className="reserved-tokens-item-empty">
+          <a onClick={() => props.onRemove(props.num)}>
+            <span className="item-remove"/>
+          </a>
+        </div>
+      }
     </div>
   );
 };
