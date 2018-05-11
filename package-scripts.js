36a6446136ece5e5a2f8a8ada1e9dbe7089f517b
Victor <Baranov.viktor.27@gmail.com>
2018-05-11 16:08:25 +0300

    Both: MintedCappedCrowdsale and Dutch Auction unit tests

--- package-scripts.js
+++ package-scripts.js
@@ -78,13 +78,28 @@ module.exports = {
     },
     test: {
       default: series(
+        'nps test.DutchAuction',
+        'nps test.MintedCappedCrowdsale',
+      ),
+      MintedCappedCrowdsale: series(
         'bash ./scripts/start_ganache.sh',
         'cd ./submodules/auth-os-applications/',
         'cd ./TokenWizard/crowdsale/MintedCappedCrowdsale/',
         'npm install',
         'node_modules/.bin/truffle migrate --network development',
         'node_modules/.bin/truffle test --network development',
-        'cd ../../',
+        'cd ../../../../../',
+        //'nps test.e2e'
+        'bash ./scripts/stop_ganache.sh',
+      ),
+      DutchAuction: series(
+        'bash ./scripts/start_ganache.sh',
+        'cd ./submodules/auth-os-applications/',
+        'cd ./TokenWizard/crowdsale/DutchCrowdsale/',
+        'npm install',
+        'node_modules/.bin/truffle migrate --network development',
+        'node_modules/.bin/truffle test --network development',
+        'cd ../../../../../',
         //'nps test.e2e'
         'bash ./scripts/stop_ganache.sh',
       ),
