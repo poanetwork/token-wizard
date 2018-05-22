96ad3812cf0712084fde963678616cf84df87ab1
fernandomg <fernando.greco@gmail.com>
2018-05-22 13:17:58 -0300

    Unify validation criteria for supply

--- src/components/Common/TokenSupply.js
+++ src/components/Common/TokenSupply.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { composeValidators, isNonNegative, isRequired } from '../../utils/validations'
+import { isPositive } from '../../utils/validations'
 import { DESCRIPTION, TEXT_FIELDS } from '../../utils/constants'
 import { Field } from 'react-final-form'
 import { InputField2 } from './InputField2'
@@ -7,14 +7,7 @@ import { acceptPositiveIntegerOnly } from '../../utils/utils'
 
 export const TokenSupply = ({ errorStyle }) => (
   <Field
-    validate={(value) => {
-      const errors = composeValidators(
-        isRequired(),
-        isNonNegative()
-      )(value)
-
-      if (errors) return errors.shift()
-    }}
+    validate={isPositive()}
     component={InputField2}
     parse={acceptPositiveIntegerOnly}
     side="right"
