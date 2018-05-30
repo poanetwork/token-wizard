460cd8662996353cfbf079b6add5b76de92a5d84
Victor <Baranov.viktor.27@gmail.com>
2018-05-30 16:43:54 +0300

    InputField2 update

--- src/components/Common/InputField2.js
+++ src/components/Common/InputField2.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { Error } from './Error'
 
-export const InputField2 = (props) => (
+export const InputField2 = (props) => {
   if (props.val) {
     props.input.value = props.val
   }
-  <div className={props.side}>
-    <label htmlFor={props.input.name} className="label">{props.label}</label>
-    <input
-      autoComplete="off"
-      className={props.inputClassName ? props.inputClassName : "input"}
-      type={props.type}
-      disabled={props.disabled}
-      placeholder={props.placeholder}
-      id={props.input.name}
-      {...props.input}
-    />
-    <p className="description">{props.description}</p>
-    <Error name={props.input.name} errorStyle={props.errorStyle}/>
-  </div>
-)
+  return (
+    <div className={props.side}>
+      <label htmlFor={props.input.name} className="label">{props.label}</label>
+      <input
+        autoComplete="off"
+        className={props.inputClassName ? props.inputClassName : "input"}
+        type={props.type}
+        disabled={props.disabled}
+        placeholder={props.placeholder}
+        id={props.input.name}
+        {...props.input}
+      />
+      <p className="description">{props.description}</p>
+      <Error name={props.input.name} errorStyle={props.errorStyle}/>
+    </div>
+  )
+}
 
