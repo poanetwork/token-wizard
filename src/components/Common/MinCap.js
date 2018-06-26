f27ce742e34121e0f0c60e60f14148a0c6b629b4
fernandomg <fernando.greco@gmail.com>
2018-06-26 18:49:58 -0300

    Make cross fields validation dependant of form's values instead of Stores

--- src/components/Common/MinCap.js
+++ src/components/Common/MinCap.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import { Field } from 'react-final-form'
 import { InputField2 } from './InputField2'
-import {
-  composeValidators,
-  isDecimalPlacesNotGreaterThan,
-  isLessOrEqualThan,
-  isNonNegative
-} from '../../utils/validations'
+import { validateTierMinCap } from '../../utils/validations'
 import { DESCRIPTION, TEXT_FIELDS } from '../../utils/constants'
 
-export const MinCap = ({ ...props }) => (
+export const MinCap = ({ index, decimals, ...props }) => (
   <Field
     component={InputField2}
-    validate={(value) => {
-      const errors = composeValidators(
-        isNonNegative(),
-        isDecimalPlacesNotGreaterThan(`Decimals should not exceed ${props.decimals} places`)(props.decimals),
-        isLessOrEqualThan(`Should be less or equal than tier's supply (${props.supply})`)(props.supply)
-      )(value)
-
-      if (errors) return errors.shift()
-    }}
-    type="number"
+    validate={props.disabled ? undefined : validateTierMinCap(decimals)(index)}
+    type="text"
     label={props.label || TEXT_FIELDS.MIN_CAP}
     description={DESCRIPTION.MIN_CAP}
     {...props}
