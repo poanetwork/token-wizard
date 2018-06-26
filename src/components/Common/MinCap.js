6bf2c358381a7ee0b0ac9db7f03b5cacc82524c1
fernandomg <fernando.greco@gmail.com>
2018-06-26 08:53:53 -0300

    Add validations for supply and update error messages

--- src/components/Common/MinCap.js
+++ src/components/Common/MinCap.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Field } from 'react-final-form'
 import { InputField2 } from './InputField2'
-import { composeValidators, isDecimalPlacesNotGreaterThan, isNonNegative } from '../../utils/validations'
+import {
+  composeValidators,
+  isDecimalPlacesNotGreaterThan,
+  isLessOrEqualThan,
+  isNonNegative
+} from '../../utils/validations'
 import { DESCRIPTION, TEXT_FIELDS } from '../../utils/constants'
 
 export const MinCap = ({ ...props }) => (
   <Field
     component={InputField2}
-    validate={composeValidators(
-      isNonNegative(),
-      isDecimalPlacesNotGreaterThan()(props.decimals)
-    )}
+    validate={(value) => {
+      const errors = composeValidators(
+        isNonNegative(),
+        isDecimalPlacesNotGreaterThan(`Decimals should not exceed ${props.decimals} places`)(props.decimals),
+        isLessOrEqualThan(`Should be less or equal than tier's supply (${props.supply})`)(props.supply)
+      )(value)
+
+      if (errors) return errors.shift()
+    }}
     type="number"
     label={props.label || TEXT_FIELDS.MIN_CAP}
     description={DESCRIPTION.MIN_CAP}
