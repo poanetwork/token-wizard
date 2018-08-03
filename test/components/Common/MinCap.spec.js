4e621bf67aaec14cbc2eeecf27875971f5151d8b
dennis00010011b <person@persons-MacBook-Pro.local>
2018-08-03 03:57:26 -0700

    MinCap test

--- test/components/Common/MinCap.spec.js
+++ test/components/Common/MinCap.spec.js
@@ -1,24 +1,26 @@
 import React from 'react'
-import { CrowdsaleRate } from '../../../src/components/Common/CrowdsaleRate'
+import { MinCap } from '../../../src/components/Common/MinCap'
 import { Form } from 'react-final-form'
 import Adapter from 'enzyme-adapter-react-15'
 import { configure, mount, shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 configure({ adapter: new Adapter() })
-const DECRIPTION = `Exchange rate Ethereum to Tokens. If it's 100, then for 1 Ether you can buy 100 tokens`
-const LABEL = `Rate`
-describe('CrowdsaleRate ', () => {
-  it(`should render CrowdsaleRate component`, () => {
+const DECRIPTION = `Minimum amount of tokens to buy. Not the minimal amount for every transaction: if minCap is 1
+               and a user already has 1 token from a previous transaction, they can buy any amount they want.`
+const LABEL = `Contributor min cap`
+
+describe('MinCap ', () => {
+  it(`should render MinCap component`, () => {
     let input = {
-      name: 'CrowdsaleRate',
+      name: 'MinCap',
       disabled: false,
       value: '1234'
     }
     const wrapper = renderer.create(
       <Form
         onSubmit={jest.fn()}
-        component={CrowdsaleRate}
+        component={MinCap}
         disabled={false}
         validate={{}}
         input={input}
@@ -26,27 +28,27 @@ describe('CrowdsaleRate ', () => {
     )
     expect(wrapper).toMatchSnapshot()
   })
-  it(`should be proper label `, () => {
+  it(`should be label ${LABEL}`, () => {
     const wrapper = mount(
       <Form
-        onSubmit={jest.fn()} component={CrowdsaleRate} validate={{}}
+        onSubmit={jest.fn()} component={MinCap} validate={{}}
       />
     )
     let lab = wrapper.find('label')
     expect(lab.text()).toBe(LABEL)
   })
-  it(`should be proper decription`, () => {
+  it(`should be decription ${DECRIPTION}`, () => {
     const wrapper = mount(
       <Form
-        onSubmit={jest.fn()} component={CrowdsaleRate} validate={{}}
+        onSubmit={jest.fn()} component={MinCap} validate={{}}
       />
     )
     let lab = wrapper.find('p[className="description"]')
     expect(lab.text()).toBe(DECRIPTION)
   })
   it(`should give error if empty`, () => {})
-  it(`should give error if not positive`, () => {})
-  it(`should give error if not integer`, () => {})
-  it(`should give error if greater than 1e18`, () => {})
+  it(`should give error if negative`, () => {})
+  it(`should give error if decimals place greater than given`, () => {})
+  it(`should give error if greater tier's supply`, () => {})
   it(`should give error if not numberic`, () => {})
 })
