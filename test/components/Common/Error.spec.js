64d407e9001854ecc326d62a505ac7c7a0a6591d
dennis00010011b <person@persons-MacBook-Pro.local>
2018-08-08 09:51:48 -0700

    Error: update test

--- test/components/Common/Error.spec.js
+++ test/components/Common/Error.spec.js
@@ -4,20 +4,33 @@ import { Form } from 'react-final-form'
 import Adapter from 'enzyme-adapter-react-15'
 import { configure, mount, shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
-import { VALIDATION_MESSAGES } from "../../../src/utils/constants";
-import {getIn} from 'final-form'
+import { FORM_ERROR } from 'final-form'
+import {validateTierMinCap} from "../../../src/utils/validations";
 
 configure({ adapter: new Adapter() })
 
 describe('Error ', () => {
+
+  const errorStyle = {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: '24px',
+    width: '50%',
+    height: '10px'
+  }
+
+
   it(`should render Error component`, () => {
-    const wrapper = renderer.create(
+    const getErrors = () =>{
+      return FORM_ERROR
+    }
+    const wrapper = mount (
       <Form
         onSubmit={jest.fn()}
         component={Error}
         name='errorName'
-
-
+        validate={getErrors}
+        errorStyle={errorStyle}
       />
     )
     expect(wrapper).toMatchSnapshot()
