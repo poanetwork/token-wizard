bfaacab525dff99d9bda5c91aa613dff7f0fb243
dennis00010011b <person@persons-MacBook-Pro.local>
2018-08-09 02:02:16 -0700

    CrowdsaleStartTime:update test

--- test/components/Common/CrowdsaleStartTime.spec.js
+++ test/components/Common/CrowdsaleStartTime.spec.js
@@ -3,48 +3,145 @@ import { CrowdsaleStartTime } from '../../../src/components/Common/CrowdsaleStar
 import { Form } from 'react-final-form'
 import Adapter from 'enzyme-adapter-react-15'
 import { configure, mount, shallow } from 'enzyme'
-import { VALIDATION_MESSAGES } from '../../../src/utils/constants'
 import renderer from 'react-test-renderer'
+import { VALIDATION_MESSAGES, TEXT_FIELDS, DESCRIPTION } from '../../../src/utils/constants';
+import MockDate from 'mockdate'
+import { InputField2 } from '../../../src/components/Common/InputField2'
 
 configure({ adapter: new Adapter() })
-const DECRIPTION_START_TIME = `Date and time when the tier starts. Can't be in the past from the current moment.`
-const LABEL = `Start Time`;
-describe('CrowdsaleStartTime', () => {
+const DECRIPTION = DESCRIPTION.START_TIME
+const LABEL = TEXT_FIELDS.START_TIME
+
+describe('CrowdsaleStartTime ', () => {
+  const crowdsale = { tiers: [{ endTime: Date().now }] }
   it(`should render CrowdsaleStartTime component`, () => {
-    let input = {
-      name: 'CrowdsaleStartTime',
-      disabled: false,
-      value: '08/02/2018'
-    }
     const wrapper = renderer.create(
       <Form
         onSubmit={jest.fn()}
         component={CrowdsaleStartTime}
-        validate={{}}
-        input={input}
+        disabled={false}
+        initialValues={crowdsale}
+        errorStyle={{ color: 'red', fontWeight: 'bold' }}
+        name="name"
+        index={0}
+        disabled={false}
       />
     )
     expect(wrapper).toMatchSnapshot()
   })
-  it(`should be label ${LABEL}`, () => {
-    const wrapper = mount(
+
+  it(`should render MinCap component if field is disabled`, () => {
+    const wrapper = renderer.create(
       <Form
-        onSubmit={jest.fn()} component={CrowdsaleStartTime} validate={{}}
+        onSubmit={jest.fn()}
+        component={CrowdsaleStartTime}
+        disabled={true}
+        initialValues={crowdsale}
+        errorStyle={{ color: 'red', fontWeight: 'bold' }}
+        name="name"
+        index={0}
       />
     )
-    let lab = wrapper.find('label')
-    expect(lab.text()).toBe(LABEL)
+    expect(wrapper).toMatchSnapshot()
   })
-  it(`should be decription ${DECRIPTION_START_TIME}`, () => {
+
+  it(`should be  proper label and description`, () => {
     const wrapper = mount(
       <Form
-        onSubmit={jest.fn()} component={CrowdsaleStartTime} validate={{}}
+        onSubmit={jest.fn()}
+        component={CrowdsaleStartTime}
+        initialValues={crowdsale}
+        errorStyle={{ color: 'red', fontWeight: 'bold' }}
+        name="name"
+        disabled={false}
+        index={0}
       />
     )
-    let lab = wrapper.find('p[className="description"]')
-    expect(lab.text()).toBe(DECRIPTION_START_TIME)
+    let label = wrapper.find('label')
+    expect(label.text()).toBe(LABEL)
+    let descript = wrapper.find('p[className="description"]')
+    expect(descript.text()).toBe(DECRIPTION)
   })
-  it(`should give error if empty`, () => {})
-  it(`should give error if data in the past`, () => {})
-  it(`should give error if later than end time`, () => {})
+})
+
+describe('CrowdsaleStartTime ', () => {
+  const TIMESTAMPS = {
+    CURRENT_TIME: 1520852400000,
+    PLUS_5_MINUTES: 1520852700000,
+    PLUS_10_MINUTES: 1520853000000,
+    PLUS_10_DAYS: 1521716400000,
+    MINUS_5_MINUTES: 1520852100000,
+    MINUS_10_DAYS: 1519988400000
+  }
+  beforeEach(() => {
+    MockDate.set(TIMESTAMPS.CURRENT_TIME)
+  })
+  afterEach(() => {
+    MockDate.reset()
+  })
+  const crowdsale = { tiers: [{ endTime: TIMESTAMPS.PLUS_10_MINUTES }] }
+  const wrapper = mount(
+    <Form
+      onSubmit={jest.fn()}
+      initialValues={crowdsale}
+      component={CrowdsaleStartTime}
+      errorStyle={{ color: 'red', fontWeight: 'bold' }}
+      name="startTime"
+      index={0}
+      disabled={false}
+    />
+  )
+  it(`shouldn't be errors if value is correct`, () => {
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: TIMESTAMPS.PLUS_5_MINUTES } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error).toBeUndefined()
+  })
+  it(`should fail if value is empty`, () => {
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: '' } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error[0]).toBe(VALIDATION_MESSAGES.REQUIRED)
+  })
+  it(`should fail if startTime is previous than current time`, () => {
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: TIMESTAMPS.MINUS_5_MINUTES } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error[0]).toBe(VALIDATION_MESSAGES.DATE_IN_FUTURE)
+  })
+
+  it(`should fail if startTime is  later than end time`, () => {
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: TIMESTAMPS.PLUS_10_DAYS } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error[0]).toBe("Should be previous than same tier's End Time")
+  })
+  it(`should fail if startTime is same with end time`, () => {
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: TIMESTAMPS.PLUS_10_MINUTES } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error[0]).toBe("Should be previous than same tier's End Time")
+  })
+
+  it(`should fail if startTime is same or later than previous tier's endTime`, () => {
+    const crowdsale = {
+      tiers: [{ endTime: TIMESTAMPS.PLUS_10_MINUTES },
+        { endTime: TIMESTAMPS.PLUS_10_DAYS }]
+    }
+    const wrapper = mount(
+      <Form
+        onSubmit={jest.fn()}
+        initialValues={crowdsale}
+        component={CrowdsaleStartTime}
+        errorStyle={{ color: 'red', fontWeight: 'bold' }}
+        name="startTime"
+        index={1}
+        disabled={false}
+      />
+    )
+    const input = wrapper.find('input[name="startTime"]')
+    input.simulate('change', { target: { value: TIMESTAMPS.PLUS_5_MINUTES } })
+    const inputProps = wrapper.find('InputField2').props()
+    expect(inputProps.meta.error[0]).toBe("Should be same or later than previous tier's End Time")
+  })
 })
