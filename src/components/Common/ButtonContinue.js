cc15b5c17e9cf5bfe021d00f093c89204f494bb5
Mariano Aguero <mariano.aguero@altoros.com>
2018-08-14 08:20:18 -0300

    Added callback to button continue

--- src/components/Common/ButtonContinue.js
+++ src/components/Common/ButtonContinue.js
@@ -7,13 +7,13 @@ export class ButtonContinue extends Component {
    * @returns {*}
    */
   render() {
-    const { status, type } = this.props
+    const { status, type, onClick } = this.props
     const submitButtonClass = classNames('button', 'button_fill', 'button_no_border', {
       button_disabled: !status
     })
 
     return (
-      <button type={type} disabled={!status} className={submitButtonClass}>
+      <button onClick={onClick} type={type} disabled={!status} className={submitButtonClass}>
         Continue
       </button>
     )
