1d5d50fa96475fb75ab5958b164a660b3022a9d8
fernandomg <fernando.greco@gmail.com>
2018-09-06 14:33:40 -0300

    Update Loader component tests

--- test/components/Common/Loader.spec.js
+++ test/components/Common/Loader.spec.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { Loader } from '../../../src/components/Common/Loader'
 import Adapter from 'enzyme-adapter-react-15'
 import { configure, shallow } from 'enzyme'
@@ -6,23 +7,24 @@ import { configure, shallow } from 'enzyme'
 configure({ adapter: new Adapter() })
 
 describe('Loader ', () => {
-  it(`should render Loader component with class name 'loading container'`, () => {
-    const wrapper = shallow(
-      <Loader
-        show={true}
-      />
-    )
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.find('[className="loading-container"]')).toBeDefined()
+  it(`should render Loader`, () => {
+    // Given
+    const component = renderer.create(<Loader show={true} />)
+
+    // When
+    const tree = component.toJSON()
+
+    // Then
+    expect(tree).toMatchSnapshot()
   })
 
   it(`should render Loader component with class name 'loading container'`, () => {
-    const wrapper = shallow(
-      <Loader
-        show={false}
-      />
-    )
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.find('[className="loading-container notdisplayed"]')).toBeDefined()
+    const wrapper = shallow(<Loader show={true} />)
+    expect(wrapper.type()).toBe('div')
+  })
+
+  it(`should render Loader component with class name 'loading container'`, () => {
+    const wrapper = shallow(<Loader show={false} />)
+    expect(wrapper.type()).toBeNull()
   })
 })
