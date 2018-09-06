dac60813c656f39cc0b7cdd663f9f67e5b545763
Mariano Aguero <mariano.aguero@altoros.com>
2018-09-06 10:20:06 -0300

    Add script to move truffle config

---
+++ scripts/moveTruffleConfig.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+
+const copyFile = (source, target, cb) => {
+  let cbCalled = false
+
+  let rd = fs.createReadStream(source)
+  rd.on('error', function(err) {
+    done(err)
+  })
+  let wr = fs.createWriteStream(target)
+  wr.on('error', err => {
+    done(err)
+  })
+  wr.on('close', ex => {
+    done()
+  })
+  rd.pipe(wr)
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err)
+      cbCalled = true
+    }
+  }
+}
+
+const copyStrategy = strategy => {
+  try {
+    const strategiesAllowed = ['Dutch', 'MintedCapped']
+    if (!strategiesAllowed.includes(strategy)) {
+      throw new Error('Strategy doesnt exist')
+    }
+    const originPath = `${__dirname}/../submodules/auth-os-applications/TokenWizard/crowdsale/${strategy}Crowdsale/truffle.js`
+    const destinyPath = `${__dirname}/../public/metadata/${strategy}CrowdsaleTruffle.js`
+
+    copyFile(originPath, destinyPath, err => {
+      if (err) {
+        console.log(`Error moving files`, err)
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const strategiesToMove = ['Dutch', 'MintedCapped']
+
+for (let strategy of strategiesToMove) {
+  copyStrategy(strategy)
+}
