962d7894d652eb17e1e0fd8a6b1cfce80c8455b3
fernandomg <fernando.greco@gmail.com>
2018-09-18 14:04:11 -0300

    Refactor executeSequentially to receive txHash and return it along with the current index

--- src/utils/executeSequentially.js
+++ src/utils/executeSequentially.js
@@ -1,17 +1,28 @@
+import logdown from 'logdown'
+
 /**
  * Executes sequentially a list of promises
  * @param {Array} list - List of promises to be executed
+ * @param {Number} startAt - index where to start list of functions execution
+ * @param {Function} cb - Callback which will be called with the overall index being executed
  * @returns {Promise} - If fails, returns the err with the index of the function in the list
  */
 const executeSequentially = (list, startAt = 0, cb = () => {}) => {
+  const logger = logdown('TW:utils:executeSequentially')
+
   return list.slice(startAt).reduce((promise, fn, index) => {
+    const overallIndex = startAt + index
+    logger.log('overallIndex:', overallIndex)
+
     return promise.then(() => {
       return Promise.resolve()
-        .then(() => {
-          cb(startAt + index)
-          return fn()
+        .then(async () => {
+          const txHash = await fn()
+          logger.log('txHash:', txHash)
+
+          cb(overallIndex, txHash)
         })
-        .catch(err => Promise.reject([err, startAt + index]))
+        .catch(err => Promise.reject([err, overallIndex]))
     })
   }, Promise.resolve())
 }
