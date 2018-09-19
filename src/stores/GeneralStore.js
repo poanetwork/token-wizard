0e750cc6e3e7f8e2dbe8496db350ba94cd966c5d
Mariano Aguero <mariano.aguero@altoros.com>
2018-09-19 16:05:56 -0300

    Add test for generalStore

--- src/stores/GeneralStore.js
+++ src/stores/GeneralStore.js
@@ -1,4 +1,4 @@
-import { observable, action, computed } from 'mobx'
+import { observable, action, isObservable, computed } from 'mobx'
 import { GAS_PRICE } from '../utils/constants'
 import autosave from './autosave'
 
@@ -15,6 +15,9 @@ class GeneralStore {
 
   @action
   setProperty = (property, value) => {
+    if (!this.hasOwnProperty(property)) {
+      throw new Error(`${property} is not declared as a property`)
+    }
     this[property] = value
   }
 
