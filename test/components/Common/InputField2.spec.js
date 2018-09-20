541d718c69d749d86bf18e5c5c8d429ac13929ed
fernandomg <fernando.greco@gmail.com>
2018-09-20 16:04:19 -0300

    Fix tests for InputField2

--- test/components/Common/InputField2.spec.js
+++ test/components/Common/InputField2.spec.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {InputField2} from '../../../src/components/Common/InputField2'
-import {Form} from 'react-final-form'
-import {Field} from 'react-final-form'
-import renderer from 'react-test-renderer'
+import { InputField2 } from '../../../src/components/Common/InputField2'
+import { Form } from 'react-final-form'
 import Adapter from 'enzyme-adapter-react-15'
-import {configure, mount, shallow} from 'enzyme'
+import { configure, mount } from 'enzyme'
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() })
 describe('InputField2 ', () => {
   it(`should render InputField2 component with specified parameters`, () => {
     const input = {
@@ -18,7 +16,7 @@ describe('InputField2 ', () => {
       <Form
         onSubmit={jest.fn()}
         component={InputField2}
-        errorStyle={{color: 'red', fontWeight: 'bold'}}
+        errorStyle={{ color: 'red', fontWeight: 'bold' }}
         placeholder={placeholder}
         input={input}
         disabled={false}
@@ -26,9 +24,7 @@ describe('InputField2 ', () => {
     )
     expect(wrapper.find('input').props().placeholder).toBe(placeholder)
     expect(wrapper.find('input').props().disabled).toBeFalsy()
-    expect(wrapper.find('label').props().htmlFor).toBe(input.name)
     expect(wrapper.find('input').props().id).toBe(input.name)
     expect(wrapper.find('Error').props().name).toBe(input.name)
   })
-
 })
