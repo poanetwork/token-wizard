0649791ae1b95f288751f3e34889d190c3b99dcc
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2018-09-21 11:23:22 -0300

    (fix) Reserved tokens list table's styling.

--- src/components/Common/ReservedTokensTable.js
+++ src/components/Common/ReservedTokensTable.js
@@ -3,26 +3,30 @@ import React from 'react'
 const ReservedTokensTable = props => {
   const reservedTokensItems = props.tokens.map((token, index) => {
     const deleteButton = token.readOnly ? null : (
-      <div className="sw-ReservedTokensTable_Delete" onClick={() => props.removeReservedToken(index)} />
+      <td className="sw-ReservedTokensTable_Column">
+        <div className="sw-ReservedTokensTable_Delete" onClick={() => props.removeReservedToken(index)} />
+      </td>
     )
 
     return (
-      <div key={index.toString()} className="sw-ReservedTokensTable_Row">
-        <div className="sw-ReservedTokensTable_Column sw-ReservedTokensTable_Column-address">{token.addr}</div>
-        <div
-          className={`sw-ReservedTokensTable_Column sw-ReservedTokensTable_Column-dimension sw-ReservedTokensTable_Column-${
-            token.dim
-          }`}
-        />
-        <div className="sw-ReservedTokensTable_Column sw-ReservedTokensTable_Column-value">{token.val}</div>
+      <tr key={index.toString()} className="sw-ReservedTokensTable_Row">
+        <td className="sw-ReservedTokensTable_Column sw-ReservedTokensTable_Column-address">{token.addr}</td>
+        <td className="sw-ReservedTokensTable_Column">
+          <div className={`sw-ReservedTokensTable_Dimension sw-ReservedTokensTable_Dimension-${token.dim}`} />
+        </td>
+        <td className="sw-ReservedTokensTable_Column sw-ReservedTokensTable_Column-value">{token.val}</td>
         {deleteButton}
-      </div>
+      </tr>
     )
   })
 
   return (
     <div className={`sw-ReservedTokensTable ${props.extraClassName}`}>
-      <div className="sw-ReservedTokensTable_Inner">{reservedTokensItems}</div>
+      <div className="sw-ReservedTokensTable_Inner">
+        <table className={`sw-ReservedTokensTable_Table`} cellPadding="0" cellSpacing="0">
+          <tbody>{reservedTokensItems}</tbody>
+        </table>
+      </div>
     </div>
   )
 }
