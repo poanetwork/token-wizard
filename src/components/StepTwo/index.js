95ea3a1e818850eee78e3cb07f8a5c2973519973
fernandomg <fernando.greco@gmail.com>
2018-09-25 16:32:40 -0300

    Refactor StepTwo components  - moved methods to components where they're required instead of having them globally available  - injected stores directly on the components that required it

--- src/components/StepTwo/index.js
+++ src/components/StepTwo/index.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react'
 import { checkWeb3 } from '../../utils/blockchainHelpers'
 import { StepNavigation } from '../Common/StepNavigation'
 import { Loader } from '../Common/Loader'
-import { clearingReservedTokens, noMoreReservedSlotAvailable } from '../../utils/alerts'
-import { NAVIGATION_STEPS, VALIDATION_TYPES } from '../../utils/constants'
+import { NAVIGATION_STEPS } from '../../utils/constants'
 import { inject, observer } from 'mobx-react'
 import { Form } from 'react-final-form'
 import { StepTwoForm } from './StepTwoForm'
 import logdown from 'logdown'
-import { sleep, toBigNumber } from '../../utils/utils'
 import setFieldTouched from 'final-form-set-field-touched'
 
 const { TOKEN_SETUP } = NAVIGATION_STEPS
-const { VALID, INVALID } = VALIDATION_TYPES
 
-// eslint-disable-next-line no-unused-vars
 const logger = logdown('TW:stepTwo:index')
 
-@inject('tokenStore', 'crowdsaleStore', 'web3Store', 'reservedTokenStore')
+@inject('tokenStore', 'crowdsaleStore', 'reservedTokenStore')
 @observer
 export class StepTwo extends Component {
   state = {
@@ -27,75 +23,29 @@ export class StepTwo extends Component {
   }
 
   async componentDidMount() {
-    const { web3Store } = this.props
-    await checkWeb3(web3Store.web3)
+    await checkWeb3()
 
     this.setState({ loading: true })
-    const tokenValues = await this.load()
+    const tokenValues = this.load()
     logger.log('Token Values', tokenValues)
     this.setState({ loading: false, tokenValues })
   }
 
-  async load() {
+  load() {
     const { tokenStore, crowdsaleStore, reservedTokenStore } = this.props
+    const isEmpty = crowdsaleStore.isDutchAuction ? tokenStore.checkIsEmptyDutch : tokenStore.checkIsEmptyMinted
 
-    await sleep(1000)
-    if (tokenStore.isEmpty(crowdsaleStore)) {
+    if (isEmpty) {
       tokenStore.addTokenSetup()
     } else {
       this.setState({
         reload: true
       })
     }
-    const token = tokenStore.getToken(crowdsaleStore)
+
+    const token = crowdsaleStore.isDutchAuction ? tokenStore.tokenDutchStructure : tokenStore.tokenMintedStructure
     reservedTokenStore.applyDecimalsToTokens(token.decimals)
     return token
-  }
-
-  removeReservedToken = index => {
-    const { reservedTokenStore } = this.props
-
-    reservedTokenStore.removeToken(index)
-  }
-
-  clearReservedTokens = async () => {
-    const { reservedTokenStore } = this.props
-
-    let result = await clearingReservedTokens()
-    if (result && result.value) {
-      reservedTokenStore.clearAll()
-    }
-    return result
-  }
-
-  validateReservedTokensList = () => {
-    const { reservedTokenStore } = this.props
-
-    let result = reservedTokenStore.validateLength
-    if (!result) {
-      noMoreReservedSlotAvailable()
-    }
-    return result
-  }
-
-  addReservedTokensItem = newToken => {
-    const { reservedTokenStore } = this.props
-
-    reservedTokenStore.addToken(newToken)
-  }
-
-  /**
-   * Callback to update the token store
-   * @param values
-   * @param errors
-   */
-  updateTokenStore = ({ values, errors }) => {
-    const { tokenStore } = this.props
-
-    Object.keys(values).forEach(key => {
-      tokenStore.setProperty(key, values[key])
-      tokenStore.updateValidity(key, errors[key] !== undefined ? INVALID : VALID)
-    })
   }
 
   /**
@@ -106,13 +56,6 @@ export class StepTwo extends Component {
   }
 
   render() {
-    const { reservedTokenStore, crowdsaleStore, tokenStore } = this.props
-    const { isMintedCappedCrowdsale } = crowdsaleStore
-    const decimals =
-      tokenStore.validToken.decimals === VALID && tokenStore.decimals >= 0
-        ? toBigNumber(tokenStore.decimals).toFixed()
-        : 0
-
     return (
       <div>
         <section className="lo-MenuBarAndContent" ref="two">
@@ -128,21 +71,12 @@ export class StepTwo extends Component {
               </div>
             </div>
             <Form
-              addReservedTokensItem={this.addReservedTokensItem}
-              clearAll={this.clearReservedTokens}
               component={StepTwoForm}
-              crowdsaleStore={crowdsaleStore}
-              decimals={decimals}
-              disableDecimals={isMintedCappedCrowdsale && !!reservedTokenStore.tokens.length}
               id="tokenData"
               initialValues={this.state.tokenValues}
               mutators={{ setFieldTouched }}
               onSubmit={this.onSubmit}
               reload={this.state.reload}
-              removeReservedToken={this.removeReservedToken}
-              tokens={reservedTokenStore.tokens}
-              updateTokenStore={this.updateTokenStore}
-              validateReservedTokensList={this.validateReservedTokensList}
               history={this.props.history}
             />
           </div>
