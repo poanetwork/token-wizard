ca91cc70362265097e2e11a858dadf3bcb5eb8cc
Mariano Aguero <mariano.aguero@altoros.com>
2018-09-26 15:43:48 -0300

    Check if http provider is listening

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ class App extends Component {
       <Router>
         <div>
           <Switch>
+            <Route exact path="/" component={crowdsaleAddr ? Crowdsale : Home} />
             <Route exact path="/crowdsale" component={Crowdsale} />
             <Route exact path="/contribute" component={Contribute} />
             <Route exact path="/stats" component={Stats} />
@@ -65,7 +66,6 @@ class App extends Component {
                     <IncompleteDeploy />
                   ) : (
                     <Switch>
-                      <Route exact path="/" component={crowdsaleAddr ? Crowdsale : Home} />
                       <Route exact path="/manage/:crowdsalePointer" component={Manage} />
                       <Route path="/1" component={StepOne} />
                       <Route path="/2" component={StepTwo} />
