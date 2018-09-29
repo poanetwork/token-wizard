2e6e78c2baf45488cb12c4a287ba7af22a542e72
fernandomg <fernando.greco@gmail.com>
2018-09-29 09:41:41 -0300

    Update executeSequentially and its tests  - supports a new callback that will be called immediately after every fn successful call

--- src/utils/executeSequentially.js
+++ src/utils/executeSequentially.js
@@ -1,26 +1,21 @@
-import logdown from 'logdown'
-
 /**
  * Executes sequentially a list of promises
  * @param {Array} list - List of promises to be executed
  * @param {Number} startAt - index where to start list of functions execution
- * @param {Function} cb - Callback which will be called with the overall index being executed
+ * @param {Function} before - Callback which will be called before executing the fn()
+ * @param {Function} after - Callback which will be called after executing the fn()
  * @returns {Promise} - If fails, returns the err with the index of the function in the list
  */
-const executeSequentially = (list, startAt = 0, cb = () => {}) => {
-  const logger = logdown('TW:utils:executeSequentially')
-
+const executeSequentially = (list, startAt = 0, before = () => {}, after = () => {}) => {
   return list.slice(startAt).reduce((promise, fn, index) => {
     const overallIndex = startAt + index
-    logger.log('overallIndex:', overallIndex)
 
     return promise.then(() => {
       return Promise.resolve()
         .then(async () => {
-          const txHash = await fn()
-          logger.log('txHash:', txHash)
-
-          cb(overallIndex, txHash)
+          before(overallIndex)
+          await fn(overallIndex)
+          after(overallIndex)
         })
         .catch(err => Promise.reject([err, overallIndex]))
     })
