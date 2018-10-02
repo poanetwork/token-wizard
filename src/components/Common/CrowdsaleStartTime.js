c5feb32c6dd6757ee3161235909e7a4acd2480f3
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2018-10-02 16:27:10 -0300

    (feature) step 3 date inputs

--- src/components/Common/CrowdsaleStartTime.js
+++ src/components/Common/CrowdsaleStartTime.js
@@ -7,10 +7,10 @@ import { DESCRIPTION, TEXT_FIELDS } from '../../utils/constants'
 export const CrowdsaleStartTime = ({ index, ...props }) => (
   <Field
     component={InputField2}
-    validate={props.disabled ? undefined : validateTierStartDate(index)}
-    type="datetime-local"
-    label={TEXT_FIELDS.START_TIME}
     description={props.description || DESCRIPTION.START_TIME}
+    label={TEXT_FIELDS.START_TIME}
+    type="datetime-local"
+    validate={props.disabled ? undefined : validateTierStartDate(index)}
     {...props}
   />
 )
