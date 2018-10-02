b48a92ca1d0e6243a262c4dd005a70a5ad7827f8
Mariano Aguero <mariano.aguero@altoros.com>
2018-10-01 22:29:48 -0300

    Add function error and process csv

--- src/utils/processReservedTokens.js
+++ src/utils/processReservedTokens.js
@@ -2,6 +2,7 @@ import { composeValidators, isAddress, isDecimalPlacesNotGreaterThan, isPositive
 import { reservedTokenStore } from '../stores'
 import logdown from 'logdown'
 import Web3 from 'web3'
+import { CSVError } from './errors'
 
 const logger = logdown('TW:processReservedTokens')
 
@@ -63,32 +64,184 @@ export const processReservedTokens = ({ rows, decimals }, cb) => {
   }
 }
 
-export const errorsCsv = data => {
-  let errorRowLength = []
+export const processCsv = ({ rows }, cb) => {
+  let called = 0
+  let reservedTokenLengthError = false
+  for (let row of rows) {
+    let validLength = reservedTokenStore.validateLength
+    if (!validLength) {
+      reservedTokenLengthError = true
+      break
+    }
+
+    let [addr, dim, val] = row
+    if (addr && dim && val) {
+      dim = dim.trim().toLowerCase()
+      val = val.trim()
+      cb({ addr, dim, val })
+
+      called++
+    }
+  }
+
+  return {
+    called: called,
+    reservedTokenLengthError: reservedTokenLengthError
+  }
+}
+
+export const errorsCsv = (data, decimals) => {
+  let errorLessRowLength = []
+  let errorHigherRowLength = []
   let errorAddress = []
+  let errorEmpty = []
+  let errorDim = []
+  let errorDimValue = []
+
+  // Check for empty
+  if (data.length === 0) {
+    const errorRow = {
+      rows: 0
+    }
+    errorEmpty.push(errorRow)
+  }
 
   for (let i = 0; i < data.length; i++) {
     const row = data[i]
 
+    // Check for higher length of rows
+    if (row.length > 3) {
+      const errorRow = {
+        line: i + 1,
+        columns: row.length
+      }
+      errorHigherRowLength.push(errorRow)
+    }
+
+    // Check for less length of row
     if (row.length < 3) {
       const errorRow = {
         line: i + 1,
         columns: row.length
       }
-      errorRowLength.push(errorRow)
+      errorLessRowLength.push(errorRow)
     }
 
-    if (row[0] && !Web3.utils.isAddress(row[0])) {
+    // Check for valid address
+    let [address, dim, val] = row
+    if (address && !Web3.utils.isAddress(address)) {
       const errorRow = {
         line: i + 1,
-        address: row[0]
+        address: address
       }
       errorAddress.push(errorRow)
     }
+
+    if (dim) {
+      dim = dim.trim().toLowerCase()
+    }
+
+    if (val) {
+      val = val.trim()
+    }
+
+    // `dim` must be either 'tokens' or 'percentage'
+    if (dim && !['tokens', 'percentage'].includes(dim)) {
+      const errorRow = {
+        line: i + 1,
+        dim: dim
+      }
+      errorDim.push(errorRow)
+    }
+
+    // `dim` must be either 'tokens' or 'percentage'
+    if (dim === 'tokens') {
+      let errorRow = {
+        line: i + 1,
+        dim: dim,
+        value: val,
+        type: 'tokens'
+      }
+      if (isRequired()(val)) {
+        errorRow.validationType = 'required'
+        errorDimValue.push(errorRow)
+      }
+      if (isPositive()(val)) {
+        errorRow.validationType = 'positive'
+        errorDimValue.push(errorRow)
+      }
+      if (isDecimalPlacesNotGreaterThan()(decimals)(val)) {
+        errorRow.validationType = 'decimalPlacesNotGreaterThan'
+        errorDimValue.push(errorRow)
+      }
+    }
+
+    if (dim === 'percentage') {
+      let errorRow = {
+        line: i + 1,
+        dim: dim,
+        value: val,
+        type: 'percentage'
+      }
+      if (isRequired()(val)) {
+        errorRow.validationType = 'required'
+        errorDimValue.push(errorRow)
+      }
+      if (isPositive()(val)) {
+        errorRow.validationType = 'positive'
+        errorDimValue.push(errorRow)
+      }
+    }
+  }
+
+  if (errorEmpty && errorEmpty.length > 0) {
+    throw new CSVError({
+      code: 100,
+      body: errorEmpty
+    })
+  }
+
+  if (errorLessRowLength && errorLessRowLength.length > 0) {
+    throw new CSVError({
+      code: 101,
+      body: errorLessRowLength
+    })
+  }
+
+  if (errorHigherRowLength && errorHigherRowLength.length > 0) {
+    throw new CSVError({
+      code: 102,
+      body: errorHigherRowLength
+    })
+  }
+
+  if (errorAddress && errorAddress.length > 0) {
+    throw new CSVError({
+      code: 103,
+      body: errorAddress
+    })
+  }
+
+  if (errorDim && errorDim.length > 0) {
+    throw new CSVError({
+      code: 104,
+      body: errorDim
+    })
+  }
+
+  if (errorDimValue && errorDimValue.length > 0) {
+    throw new CSVError({
+      code: 105,
+      body: errorDimValue
+    })
   }
 
   return {
-    errorRowLength: errorRowLength,
-    errorAddress: errorAddress
+    errorEmpty: errorEmpty,
+    errorLessRowLength: errorLessRowLength,
+    errorHigherRowLength: errorHigherRowLength,
+    errorAddress: errorAddress,
+    errorDim: errorDim,
+    errorDimValue: errorDimValue
   }
 }
