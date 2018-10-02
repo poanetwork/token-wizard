fea67e6f0f9a9628fa3385d8f1c842304852bb46
Mariano Aguero <mariano.aguero@altoros.com>
2018-10-01 22:28:47 -0300

    Add CSVError class

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -6,3 +6,13 @@ export class Web3Error extends Error {
     this.code = code
   }
 }
+
+export class CSVError extends Error {
+  constructor(data) {
+    const { code, body, message } = data
+    super(message)
+
+    this.code = code
+    this.body = body
+  }
+}
