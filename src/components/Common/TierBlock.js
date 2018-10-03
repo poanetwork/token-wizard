39a02aaabddc89f06adb27547a3c7574fad45427
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2018-10-03 16:09:28 -0300

    (feature) White list elements / select buttons elements / tier block elements / refactored some components.

--- src/components/Common/TierBlock.js
+++ src/components/Common/TierBlock.js
@@ -9,7 +9,7 @@ import { CrowdsaleEndTime } from './CrowdsaleEndTime'
 import { CrowdsaleRate } from './CrowdsaleRate'
 import { Supply } from './Supply'
 import { MinCap } from './MinCap'
-import { RadioInputField } from './RadioInputField'
+import { FormControlTitle } from '../Common/FormControlTitle'
 
 const { ALLOW_MODIFYING, CROWDSALE_SETUP_NAME, ENABLE_WHITELISTING } = TEXT_FIELDS
 
@@ -19,99 +19,119 @@ export const TierBlock = ({ fields, ...props }) => {
     if (props.tierStore) {
       props.tierStore.emptyWhitelist(index)
     }
+
     return input.onChange(value)
   }
 
+  const tierBlockSelectButtons = (title, description, buttons) => {
+    return (
+      <div className="sw-TierBlock_SelectButtons">
+        <FormControlTitle title={title} description={description} />
+        <div className="sw-TierBlock_SelectButtonsContainer">
+          {buttons.map((item, index) => (
+            <label className="sw-TierBlock_SelectButtonsLabel" key={index}>
+              <input
+                checked={item.checked}
+                className="sw-TierBlock_SelectButtonsInput"
+                id={item.id}
+                name={item.name}
+                onChange={item.onChange}
+                type="radio"
+                value={item.value}
+              />
+              <span className="sw-TierBlock_SelectButtonsButton">{item.label}</span>
+            </label>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  const getWhiteListingButtons = (name, input, index) => {
+    const buttons = [
+      {
+        checked: input.value === 'yes',
+        id: `${name}.enable_whitelisting_yes`,
+        label: 'Yes',
+        name: name,
+        onChange: () => onChangeWhitelisted('yes', input, index),
+        value: 'yes'
+      },
+      {
+        checked: input.value === 'no',
+        id: `${name}.enable_whitelisting_no`,
+        label: 'No',
+        name: name,
+        onChange: () => onChangeWhitelisted('no', input, index),
+        value: 'no'
+      }
+    ]
+
+    return buttons
+  }
+
+  const getAllowModifiyingButtons = (name, input) => {
+    const buttons = [
+      {
+        checked: input.value === 'on',
+        id: `${name}.allow_modifying_on`,
+        label: 'On',
+        name: name,
+        onChange: () => input.onChange('on'),
+        value: 'on'
+      },
+      {
+        checked: input.value === 'off',
+        id: `${name}.allow_modifying_off`,
+        label: 'Off',
+        name: name,
+        onChange: () => input.onChange('off'),
+        value: 'off'
+      }
+    ]
+
+    return buttons
+  }
+
   return (
-    <div>
+    <div className="sw-TierBlock">
       {fields.map((name, index) => (
         <div className="sw-BorderedBlock" key={index}>
           <Field
-            id={`${name}.tier`}
-            name={`${name}.tier`}
-            validate={value => {
-              const errors = composeValidators(isRequired(), isMaxLength()(30))(value)
-
-              if (errors) return errors.shift()
-            }}
             component={InputField2}
             description={DESCRIPTION.CROWDSALE_SETUP_NAME}
+            id={`${name}.tier`}
             label={CROWDSALE_SETUP_NAME}
+            name={`${name}.tier`}
             type="text"
+            validate={value => {
+              const errors = composeValidators(isRequired(), isMaxLength()(30))(value)
+              if (errors) return errors.shift()
+            }}
           />
           <CrowdsaleStartTime disabled={index > 0} index={index} name={`${name}.startTime`} />
           <CrowdsaleRate name={`${name}.rate`} />
-          {/* <RadioInputField
-            extraClassName="sw-BorderedBlock_Row1Column2"
-            items={[{ label: 'Tokens', value: 'tokens' }, { label: 'Percentage', value: 'percentage' }]}
-            onChange={e => {this.updateReservedTokenInput(e.target.value, 'dim')}}
-            selectedItem={this.state.dim}
-            title={'asdf'}
-            description="Fixed amount or % of crowdsaled tokens. Will be deposited to the account after finalization
-              of the crowdsale."
-          /> */}
           <Field
+            id={`${name}.whitelistEnabled`}
             name={`${name}.whitelistEnabled`}
-            render={({ input }) => (
-              <div className="right">
-                <label className="label">{ENABLE_WHITELISTING}</label>
-                <div className="radios-inline">
-                  <label className="radio-inline">
-                    <input
-                      id={`${name}.enable_whitelisting_yes`}
-                      type="radio"
-                      checked={input.value === 'yes'}
-                      value="yes"
-                      onChange={() => onChangeWhitelisted('yes', input, index)}
-                    />
-                    <span className="title">yes</span>
-                  </label>
-                  <label className="radio-inline">
-                    <input
-                      id={`${name}.enable_whitelisting_no`}
-                      type="radio"
-                      checked={input.value === 'no'}
-                      value="no"
-                      onChange={() => onChangeWhitelisted('no', input, index)}
-                    />
-                    <span className="title">no</span>
-                  </label>
-                </div>
-                <p className="description">{DESCRIPTION.ENABLE_WHITELIST}</p>
-              </div>
-            )}
+            render={({ input }) =>
+              tierBlockSelectButtons(
+                ENABLE_WHITELISTING,
+                DESCRIPTION.ENABLE_WHITELIST,
+                getWhiteListingButtons(`${name}.whitelistEnabled`, input, index)
+              )
+            }
           />
           <Field
             id={`${name}.updatable`}
             name={`${name}.updatable`}
-            render={({ input }) => (
-              <div className="left">
-                <label className="label">{ALLOW_MODIFYING}</label>
-                <div className="radios-inline">
-                  <label className="radio-inline">
-                    <input
-                      id={`${name}.allow_modifying_on`}
-                      type="radio"
-                      checked={input.value === 'on'}
-                      onChange={() => input.onChange('on')}
-                      value="on"
-                    />
-                    <span className="title">on</span>
-                  </label>
-                  <label className="radio-inline">
-                    <input
-                      id={`${name}.allow_modifying_off`}
-                      type="radio"
-                      checked={input.value === 'off'}
-                      value="off"
-                      onChange={() => input.onChange('off')}
-                    />
-                    <span className="title">off</span>
-                  </label>
-                </div>
-                <p className="description">{DESCRIPTION.ALLOW_MODIFYING}</p>
-              </div>
-            )}
+            render={({ input }) =>
+              tierBlockSelectButtons(
+                ALLOW_MODIFYING,
+                DESCRIPTION.ALLOW_MODIFYING,
+                getAllowModifiyingButtons(`${name}.updatable`, input)
+              )
+            }
           />
           <CrowdsaleEndTime name={`${name}.endTime`} index={index} side="right" />
           <Supply
@@ -123,17 +143,9 @@ export const TierBlock = ({ fields, ...props }) => {
               (props.tierStore && props.tierStore.tiers[index].whitelist.length)
             }
           />
-          <MinCap
-            name={`${name}.minCap`}
-            decimals={props.decimals}
-            index={index}
-            disabled={props.tierStore ? props.tierStore.tiers[index].whitelistEnabled === 'yes' : true}
-          />
           {props.tierStore.tiers[index].whitelistEnabled === 'yes' ? (
             <div>
-              <div className="section-title">
-                <p className="title">Whitelist</p>
-              </div>
+              <MinCap decimals={props.decimals} index={index} name={`${name}.minCap`} />
               <WhitelistInputBlock num={index} decimals={props.decimals} />
             </div>
           ) : null}
