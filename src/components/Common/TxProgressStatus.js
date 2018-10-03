d349b2e012f6d10209519720a759614e9132073f
fernandomg <fernando.greco@gmail.com>
2018-10-03 16:52:43 -0300

    Update `TxProgressStatus` to display step status

--- src/components/Common/TxProgressStatus.js
+++ src/components/Common/TxProgressStatus.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { observer, inject } from 'mobx-react'
+import { inject, observer } from 'mobx-react'
 import '../../assets/stylesheets/application.css'
 import { TX_STEP_DESCRIPTION } from '../stepFour/constants'
+import classNames from 'classnames'
 
 @inject('tierStore', 'deploymentStore')
 @observer
@@ -21,14 +22,22 @@ export class TxProgressStatus extends Component {
     return table
   }
 
-  txActivity = ({ active, confirmationPending, miningPending, mined }, index) => {
+  isConstructing = ({ active, confirmationPending }) => active && !confirmationPending
+  isConfirmationPending = ({ confirmationPending, miningPending }) => confirmationPending && !miningPending
+  isMiningPending = ({ miningPending, mined }) => miningPending && !mined
+
+  txActivity = (status, index) => {
     let statusMessage = ''
 
-    if (active && !confirmationPending) statusMessage = 'constructing tx...'
-    if (confirmationPending && !miningPending) statusMessage = 'please confirm tx...'
-    if (miningPending && !mined) statusMessage = 'tx pending of being mined...'
+    if (this.isConstructing(status)) statusMessage = 'constructing tx...'
+    if (this.isConfirmationPending(status)) statusMessage = 'please confirm tx...'
+    if (this.isMiningPending(status)) statusMessage = 'tx pending of being mined...'
 
-    return statusMessage === '' ? null : <span key={index.toString()}>{statusMessage}</span>
+    return statusMessage === '' ? null : (
+      <span className="tx-status" key={index.toString()}>
+        {statusMessage}
+      </span>
+    )
   }
 
   render() {
@@ -42,8 +51,8 @@ export class TxProgressStatus extends Component {
           <div className="table-row flex-table-header">
             <div className="text">Tx Name</div>
             {whitelisted.map(
-              (value, index) =>
-                value || index === 0 ? (
+              (tierWhitelisted, index) =>
+                tierWhitelisted || index === 0 ? (
                   <div className="sm-text" key={index.toString()}>
                     Tier {index + 1}
                   </div>
@@ -54,18 +63,29 @@ export class TxProgressStatus extends Component {
             {tableContent.map(
               tx =>
                 tx.status.length ? (
-                  <div className="table-row datagrid" key={tx.name}>
+                  <div
+                    className={classNames('table-row', 'datagrid', {
+                      active: tx.status.some(
+                        status =>
+                          status
+                            ? this.isMiningPending(status) ||
+                              this.isConfirmationPending(status) ||
+                              this.isConstructing(status)
+                            : false
+                      )
+                    })}
+                    key={tx.name}
+                  >
                     <div className="text">
-                      {TX_STEP_DESCRIPTION[tx.name]} {tx.status.map((status, index) => this.txActivity(status, index))}
+                      {TX_STEP_DESCRIPTION[tx.name]}{' '}
+                      {tx.status.map((status, index) => (status ? this.txActivity(status, index) : null))}
                     </div>
                     {whitelisted.map(
                       (tierWhitelisted, index) =>
-                        tierWhitelisted ? (
+                        tierWhitelisted || index === 0 ? (
                           <div className="sm-text" key={index.toString()}>
-                            {tx.status.length > index ? (
-                              <i className="material-icons">
-                                {tx.status[index].mined === true ? 'check' : 'access_time'}
-                              </i>
+                            {tx.status[index] ? (
+                              <i className="material-icons">{tx.status[index].mined ? 'check' : 'access_time'}</i>
                             ) : (
                               ''
                             )}
