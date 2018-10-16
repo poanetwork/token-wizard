60c3d54f35f11c1dfffc7c1f4590b36e2651d54e
Mariano Aguero <mariano.aguero@altoros.com>
2018-10-16 10:07:01 -0300

    Remove double dots

--- src/components/Common/NoWeb3.js
+++ src/components/Common/NoWeb3.js
@@ -10,7 +10,7 @@ export default class NoWeb3 extends Component {
               <h1 className="title">Wallet not found, or access to Ethereum account not granted</h1>
               <p className="description">
                 If a Wallet extension is installed on your web browser, please verify that the access to Ethereum account has been granted and is available for the corresponding domain.
-                . Check Token Wizard GitHub for <a href='https://github.com/poanetwork/token-wizard' target='blank'>the instruction</a>.
+                Check Token Wizard GitHub for <a href='https://github.com/poanetwork/token-wizard' target='blank'>the instruction</a>.
               </p>
             </div>
           </div>
