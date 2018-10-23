f85ff5c95176728b9ed80b8923228edd884878ba
Mariano Aguero <mariano.aguero@altoros.com>
2018-10-23 20:21:29 -0300

    Refactor with path.join

--- scripts/moveTruffleConfig.js
+++ scripts/moveTruffleConfig.js
@@ -2,6 +2,7 @@
  * Script that move truffle config to a public folder
  */
 
+const path = require('path')
 const { copyFile } = require('./helpers/utils')
 
 const directory = process.argv.slice(2)[0] || 'public'
@@ -12,12 +13,17 @@ const copyStrategy = strategy => {
     if (!strategiesAllowed.includes(strategy)) {
       throw new Error('Strategy doesnt exist')
     }
-    const originPath = `${__dirname}/../submodules/auth-os-applications/TokenWizard/crowdsale/${strategy}Crowdsale/truffle.js`
-    const destinyPath = `${__dirname}/../${directory}/metadata/${strategy}CrowdsaleTruffle.js`
+    const originPath = path.join(
+      __dirname,
+      `../submodules/auth-os-applications/TokenWizard/crowdsale/${strategy}Crowdsale/truffle.js`
+    )
+    const destinyPath = path.join(__dirname, `../${directory}/metadata/${strategy}CrowdsaleTruffle.js`)
 
     copyFile(originPath, destinyPath, err => {
       if (err) {
         console.log(`Error moving files`, err)
+      } else {
+        console.log(`Move ${originPath} - Destiny ${destinyPath}`)
       }
     })
   } catch (e) {
