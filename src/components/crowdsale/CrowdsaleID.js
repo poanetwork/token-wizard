4446ad408deab9fa296fca645282ff731e86b5aa
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2018-11-01 17:13:28 -0300

    (fix) added copy button to crowdsale page id / hash

--- src/components/crowdsale/CrowdsaleID.js
+++ src/components/crowdsale/CrowdsaleID.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { ButtonCopyToClipboard } from '../Common/ButtonCopyToClipboard'
 
 export const CrowdsaleID = ({ hash, description, extraClassname = '' }) => {
   return (
     <div className={`cs-CrowdsaleID ${extraClassname}`}>
       <div className="cs-CrowdsaleID_InnerContainer">
-        <h3 className="cs-CrowdsaleID_Hash">{hash}</h3>
+        <h3 className="cs-CrowdsaleID_Hash">
+          {hash} <ButtonCopyToClipboard value={hash} />
+        </h3>
         <p className="cs-CrowdsaleID_Description">{description}</p>
       </div>
     </div>
