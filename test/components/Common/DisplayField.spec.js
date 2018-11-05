10c079079d5b497b351eaa232db7769f9c5adbcb
fernandomg <fernando.greco@gmail.com>
2018-11-05 12:49:15 -0300

    Fix tests after merge

--- test/components/Common/DisplayField.spec.js
+++ test/components/Common/DisplayField.spec.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import {DisplayField} from '../../../src/components/Common/DisplayField'
-import {Form} from 'react-final-form'
+import { DisplayField } from '../../../src/components/Common/DisplayField'
+import { Form } from 'react-final-form'
 import Adapter from 'enzyme-adapter-react-15'
-import {configure, mount, shallow} from 'enzyme'
+import { configure, mount } from 'enzyme'
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() })
+
 describe('DisplayField ', () => {
   it(`should render DisplayField component with specified parameters`, () => {
     const description = 'DisplayField description'
     const title = 'DisplayField title'
     const value = 'DisplayField value'
-    const side = 'DisplayField'
+    const extraClass = 'anExtraClass'
+    const mobileTextSize = 'large'
+    const valueSize = {
+      small: 'Small',
+      medium: 'Medium',
+      large: 'Large',
+      extralarge: 'ExtraLarge'
+    }
     const wrapper = mount(
       <Form
         onSubmit={jest.fn()}
@@ -18,13 +26,20 @@ describe('DisplayField ', () => {
         description={description}
         title={title}
         value={value}
-        side={side}
+        extraClass={extraClass}
+        mobileTextSize={mobileTextSize}
       />
     )
-    expect(wrapper.find('[className="label"]').text()).toBe(title)
-    expect(wrapper.find('[className="value"]').text()).toBe(value)
-    expect(wrapper.find('[className="description"]').text()).toBe(description)
-    expect(wrapper.find(`[className="${side}"]`).text()).toBeDefined()
+    expect(wrapper.find('.pb-DisplayField').props().title).toBe(description)
+    expect(wrapper.find(`.${extraClass}`)).toHaveLength(1)
+    expect(wrapper.find('.pb-DisplayField_Title').text()).toBe(title)
+    expect(
+      wrapper
+        .find('.pb-DisplayField_Value')
+        .text()
+        .trim()
+    ).toBe(value)
+    expect(wrapper.find(`.pb-DisplayField_Value-MobileTextSize${valueSize[mobileTextSize]}`)).toHaveLength(1)
     expect(wrapper.find(`[data-clipboard-text="${value}"]`).text()).toBeDefined()
   })
 })
