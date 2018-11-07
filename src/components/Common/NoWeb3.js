5d0afd2e9a8bf821643e862f2111501301e12a00
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2018-11-07 10:12:24 -0300

    (feature) added noweb3 component

--- src/components/Common/NoWeb3.js
+++ src/components/Common/NoWeb3.js
@@ -3,33 +3,29 @@ import React, { Component } from 'react'
 export default class NoWeb3 extends Component {
   render() {
     return (
-      <div>
-        <section className="home">
-          <div className="container">
-            <div className="st-StepContent_Info">
-              <div className="st-StepContentInfo_InfoText">
-                <h1 className="st-StepContent_InfoTitle">
-                  Wallet not found, or access to Ethereum account not granted
-                </h1>
-                <p className="st-StepContent_InfoDescription">
-                  If{' '}
-                  <a
-                    href="https://chrome.google.com/webstore/detail/nifty-wallet/jbdaocneiiinmjbjlgalhcelgbejmnid"
-                    target="blank"
-                  >
-                    a Wallet
-                  </a>{' '}
-                  extension is installed on your web browser, please verify that the access to Ethereum account has been
-                  granted and is available for the corresponding domain. Check Token Wizard GitHub for{' '}
-                  <a href="https://github.com/poanetwork/token-wizard/wiki" target="blank">
-                    the instruction
-                  </a>.
-                </p>
-              </div>
+      <section className="sw-NoWeb3">
+        <div className="container">
+          <div className="st-StepContent_Info">
+            <div className="st-StepContentInfo_InfoText">
+              <h1 className="st-StepContent_InfoTitle">Wallet not found, or access to Ethereum account not granted</h1>
+              <p className="st-StepContent_InfoDescription">
+                If{' '}
+                <a
+                  href="https://chrome.google.com/webstore/detail/nifty-wallet/jbdaocneiiinmjbjlgalhcelgbejmnid"
+                  target="blank"
+                >
+                  a Wallet
+                </a>{' '}
+                extension is installed on your web browser, please verify that the access to Ethereum account has been
+                granted and is available for the corresponding domain. Check Token Wizard GitHub for{' '}
+                <a href="https://github.com/poanetwork/token-wizard/wiki" target="blank">
+                  the instruction
+                </a>.
+              </p>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     )
   }
 }
