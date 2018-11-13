73f8f0cf3fe49b0ee978800a804ff3a5642f83f9
Mariano Aguero <mariano.aguero@altoros.com>
2018-11-12 21:05:51 -0300

    Add tests for crowdsale, manage and stats

---
+++ test/components/stats/index.spec.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-15'
+import { Provider } from 'mobx-react'
+import { configure } from 'enzyme'
+import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router'
+import { Stats } from '../../../src/components/stats/index'
+import { web3Store, statsStore } from '../../../src/stores'
+
+configure({ adapter: new Adapter() })
+
+describe('Stats index', () => {
+  const stores = {
+    web3Store,
+    statsStore
+  }
+
+  it(`should render Stats`, () => {
+    // Given
+    const component = renderer.create(
+      <Provider {...stores}>
+        <MemoryRouter initialEntries={['/stat']}>
+          <Stats />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    // When
+    const tree = component.toJSON()
+
+    // Then
+    expect(tree).toMatchSnapshot()
+  })
+})
