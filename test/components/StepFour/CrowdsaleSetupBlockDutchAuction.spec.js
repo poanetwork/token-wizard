d4a5e050123fcde0ba0d73f2a21a529e99ff1960
Mariano Aguero <mariano.aguero@altoros.com>
2018-11-25 23:40:04 -0300

    Add CrowdsaleSetupBlockDutchAuction test

---
+++ test/components/StepFour/CrowdsaleSetupBlockDutchAuction.spec.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-15'
+import { configure, shallow } from 'enzyme'
+import { CrowdsaleSetupBlockDutchAuction } from '../../../src/components/StepFour/CrowdsaleSetupBlockDutchAuction'
+import { tierStore } from '../../../src/stores'
+import { defaultTier, defaultTierValidations } from '../../../src/utils/constants'
+
+configure({ adapter: new Adapter() })
+
+describe('CrowdsalSetupBlockDutchAuction', () => {
+  it(`should render the component `, () => {
+    const addCrowdsale = num => {
+      const newTier = Object.assign({}, defaultTier)
+      const newTierValidations = Object.assign({}, defaultTierValidations)
+      newTier.tier = `Tier ${num + 1}`
+      if (0 === num) {
+        newTier.whitelistEnabled = 'no'
+        newTier.burnExcess = 'no'
+        newTier.walletAddress = '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1'
+        newTier.endTime = 1234
+      }
+
+      tierStore.addTier(newTier, newTierValidations)
+    }
+
+    addCrowdsale(0)
+
+    const stores = {
+      tierStore: tierStore
+    }
+    const wrapper = shallow(<CrowdsaleSetupBlockDutchAuction {...stores} />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
